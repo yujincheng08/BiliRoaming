0a0a233627844be928ce40e86b1b1bef62331e1b
iAcn <iacnlyx@gmail.com>
2019-03-23 22:00:02 +0800

    Use preference instead of layout

--- app/src/main/java/me/iacn/biliroaming/MainActivity.java
+++ app/src/main/java/me/iacn/biliroaming/MainActivity.java
@@ -2,11 +2,24 @@ package me.iacn.biliroaming;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 
+/**
+ * Created by iAcn on 2019/3/23
+ * Email i@iacn.me
+ */
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.prefs_setting);
+        }
     }
 }
