19c9beb687628a6b5c9f526ec780206680278d23
iAcn <iacnlyx@gmail.com>
2019-03-23 21:45:33 +0800

    Remove android support package

--- app/src/main/java/me/iacn/biliroaming/MainActivity.java
+++ app/src/main/java/me/iacn/biliroaming/MainActivity.java
@@ -1,13 +1,12 @@
 package me.iacn.biliroaming;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
