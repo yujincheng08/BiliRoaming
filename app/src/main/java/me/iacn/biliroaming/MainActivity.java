0fe7956e8e9b215010efde5eea18b832cee469ca
iAcn <iacnlyx@gmail.com>
2019-03-23 21:07:23 +0800

    Init project

---
+++ app/src/main/java/me/iacn/biliroaming/MainActivity.java
@@ -0,0 +1,13 @@
+package me.iacn.biliroaming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
