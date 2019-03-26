e19ce193bff7628de2893c4027015234435fbddb
iAcn <iacnlyx@gmail.com>
2019-03-26 19:30:06 +0800

    Try to hook BangumiDetailActivity

--- app/src/main/java/me/iacn/biliroaming/XposedInit.java
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.java
@@ -7,15 +7,17 @@ import android.app.Instrumentation;
 import android.content.Context;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.List;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.getBooleanField;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 
 /**
@@ -23,6 +25,9 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
  * Email i@iacn.me
  */
 public class XposedInit implements IXposedHookLoadPackage {
+
+    private static final String TAG = "BiliRoaming";
+
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
         if (!"tv.danmaku.bili".equals(lpparam.packageName)) return;
@@ -37,38 +42,53 @@ public class XposedInit implements IXposedHookLoadPackage {
                     // ============== START ==============
                     System.out.println("============== START ==============");
 
-                    try {
-                        XposedHelpers.findAndHookMethod("b.efu", lpparam.classLoader,
-                                "a", "okhttp3.t", new XC_MethodHook() {
-                                    @Override
-                                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                                        // get OkHttpClientBuilder object
-                                        Object builder = XposedHelpers.callMethod(param.thisObject, "e");
-                                        List interceptors = (List) XposedHelpers.callMethod(builder, "a");
+                    Class<?> activityClass = findClass(
+                            "com.bilibili.bangumi.ui.detail.BangumiDetailActivity", lpparam.classLoader);
+                    Class<?> seasonClass = findClass(
+                            "com.bilibili.bangumi.api.uniform.BangumiUniformSeason", lpparam.classLoader);
 
-                                        boolean hasAdded = false;
-                                        for (Object interceptor : interceptors) {
-                                            String simpleName = interceptor.getClass().getSimpleName();
-                                            if (simpleName.contains("$Proxy")) {
-                                                hasAdded = true;
-                                                break;
-                                            }
-                                        }
+                    findAndHookMethod(activityClass, "a", seasonClass, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            Object bangumiSeason = param.args[0];
+                            List episodes = (List) getObjectField(bangumiSeason, "episodes");
+                            Object rights = getObjectField(bangumiSeason, "rights");
+                            boolean areaLimit = getBooleanField(rights, "areaLimit");
 
-                                        if (!hasAdded) {
-                                            Class<?> InterceptorInterface = XposedHelpers.findClass(
-                                                    "okhttp3.t", lpparam.classLoader);
-                                            Method addInterceptorMethod = XposedHelpers.findMethodExact(builder.getClass(),
-                                                    "a", InterceptorInterface);
-                                            Object instance = Proxy.newProxyInstance(lpparam.classLoader, new Class[]{
-                                                    InterceptorInterface}, new BangumiInterceptor2(lpparam.classLoader));
+                            if (areaLimit && episodes.size() == 0) {
+                                /*System.out.println("Limited Bangumi: seasonTitle = " +
+                                        getObjectField(bangumiSeason, "seasonTitle") +
+                                        ", seasonId = " + getObjectField(bangumiSeason, "seasonId"));*/
+                            }
+                        }
+                    });
 
-                                            addInterceptorMethod.invoke(builder, instance);
-                                        }
+//                    Class<?> clazz = findClass("com.bilibili.bangumi.viewmodel.detail.BangumiDetailViewModel", lpparam.classLoader);
+                    for (Method method : activityClass.getDeclaredMethods()) {
+                        XposedBridge.hookMethod(method, new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                System.out.println("--------------" + param.method.getName() + "--------------");
+
+                                if (param.method.getName().equals("onEvent")) {
+                                    System.out.println("param = " + param.args[0]);
+                                    Object[] objArr = (Object[]) param.args[1];
+                                    for (int i = 0; i < objArr.length; i++) {
+                                        System.out.println("param = " + objArr[i]);
                                     }
-                                });
-                    } catch (Exception e) {
-                        e.printStackTrace();
+
+                                } else {
+                                    for (int i = 0; i < param.args.length; i++) {
+                                        System.out.println("param = " + param.args[i]);
+                                    }
+                                }
+                            }
+
+                            @Override
+                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                System.out.println("Return = " + param.getResult());
+                            }
+                        });
                     }
 
                     // ============== END ==============
