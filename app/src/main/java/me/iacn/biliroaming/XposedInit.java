d882c820a6d06e56bf7beb81c3e9c87534486997
iAcn <iacnlyx@gmail.com>
2019-03-27 11:49:03 +0800

    Hook main process and download process

--- app/src/main/java/me/iacn/biliroaming/XposedInit.java
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.java
@@ -6,18 +6,13 @@ import android.app.Application;
 import android.app.Instrumentation;
 import android.content.Context;
 
-import java.lang.reflect.Method;
-import java.util.List;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import me.iacn.biliroaming.hooker.BangumiPlayUrlHook;
+import me.iacn.biliroaming.hooker.BangumiSeasonHook;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
-import static de.robv.android.xposed.XposedHelpers.findClass;
-import static de.robv.android.xposed.XposedHelpers.getBooleanField;
-import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 
 /**
@@ -25,8 +20,6 @@ import static de.robv.android.xposed.XposedHelpers.getObjectField;
  * Email i@iacn.me
  */
 public class XposedInit implements IXposedHookLoadPackage {
-
-    private static final String TAG = "BiliRoaming";
 
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
@@ -36,75 +29,26 @@ public class XposedInit implements IXposedHookLoadPackage {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 Context context = (Context) param.args[0];
-                // Only hook on the main process
-                if (isMainProcess(context)) {
+                String currentProcessName = getCurrentProcessName(context);
 
-                    // ============== START ==============
-                    System.out.println("============== START ==============");
-
-                    Class<?> activityClass = findClass(
-                            "com.bilibili.bangumi.ui.detail.BangumiDetailActivity", lpparam.classLoader);
-                    Class<?> seasonClass = findClass(
-                            "com.bilibili.bangumi.api.uniform.BangumiUniformSeason", lpparam.classLoader);
-
-                    findAndHookMethod(activityClass, "a", seasonClass, new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            Object bangumiSeason = param.args[0];
-                            List episodes = (List) getObjectField(bangumiSeason, "episodes");
-                            Object rights = getObjectField(bangumiSeason, "rights");
-                            boolean areaLimit = getBooleanField(rights, "areaLimit");
-
-                            if (areaLimit && episodes.size() == 0) {
-                                /*System.out.println("Limited Bangumi: seasonTitle = " +
-                                        getObjectField(bangumiSeason, "seasonTitle") +
-                                        ", seasonId = " + getObjectField(bangumiSeason, "seasonId"));*/
-                            }
-                        }
-                    });
-
-//                    Class<?> clazz = findClass("com.bilibili.bangumi.viewmodel.detail.BangumiDetailViewModel", lpparam.classLoader);
-                    for (Method method : activityClass.getDeclaredMethods()) {
-                        XposedBridge.hookMethod(method, new XC_MethodHook() {
-                            @Override
-                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                System.out.println("--------------" + param.method.getName() + "--------------");
-
-                                if (param.method.getName().equals("onEvent")) {
-                                    System.out.println("param = " + param.args[0]);
-                                    Object[] objArr = (Object[]) param.args[1];
-                                    for (int i = 0; i < objArr.length; i++) {
-                                        System.out.println("param = " + objArr[i]);
-                                    }
-
-                                } else {
-                                    for (int i = 0; i < param.args.length; i++) {
-                                        System.out.println("param = " + param.args[i]);
-                                    }
-                                }
-                            }
-
-                            @Override
-                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                                System.out.println("Return = " + param.getResult());
-                            }
-                        });
-                    }
-
-                    // ============== END ==============
+                // Hook main process and download process
+                if ("tv.danmaku.bili".equals(currentProcessName)) {
+                    new BangumiSeasonHook(lpparam.classLoader).startHook();
+                } else if ("tv.danmaku.bili:download".equals(currentProcessName)) {
+                    new BangumiPlayUrlHook(lpparam.classLoader).startHook();
                 }
             }
         });
     }
 
-    private boolean isMainProcess(Context context) {
+    private String getCurrentProcessName(Context context) {
         int myPid = android.os.Process.myPid();
         ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
         for (RunningAppProcessInfo processInfo : manager.getRunningAppProcesses()) {
-            if (processInfo.pid == myPid && "tv.danmaku.bili".equals(processInfo.processName)) {
-                return true;
+            if (processInfo.pid == myPid) {
+                return processInfo.processName;
             }
         }
-        return false;
+        return null;
     }
 }
