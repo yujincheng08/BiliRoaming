a61fadd7b0d3e9f69011dbb28fac4f0ca8fda2b9
LoveSy <shana@zju.edu.cn>
2020-08-24 15:38:42 +0800

    Try to fix comment floor in some cases

--- app/src/main/java/me/iacn/biliroaming/hook/CommentHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/CommentHook.kt
@@ -1,8 +1,8 @@
 package me.iacn.biliroaming.hook
 
+import de.robv.android.xposed.XC_MethodHook.MethodHookParam
 import me.iacn.biliroaming.XposedInit
-import me.iacn.biliroaming.utils.Log
-import me.iacn.biliroaming.utils.replaceMethod
+import me.iacn.biliroaming.utils.*
 
 /**
  * Created by iAcn on 2020/2/27
@@ -15,6 +15,15 @@ class CommentHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         "com.bilibili.app.comm.comment2.model.BiliCommentConfig".replaceMethod(mClassLoader, "isShowFloor") {
             return@replaceMethod true
         }
+
+        // For some cases where the above hooker is not working because of the short method
+        val hooker: (MethodHookParam) -> Unit = { param ->
+            param.thisObject.getObjectField("config")?.setIntField("mShowFloor", 1)
+        }
+        "com.bilibili.app.comm.comment2.model.BiliCommentList".hookBeforeMethod(mClassLoader, "isShowFloor", hooker = hooker)
+        "com.bilibili.app.comm.comment2.model.BiliCommentCursorList".hookBeforeMethod(mClassLoader, "isShowFloor", hooker = hooker)
+        "com.bilibili.app.comm.comment2.model.BiliCommentDialogue".hookBeforeMethod(mClassLoader, "isShowFloor", hooker = hooker)
+        "com.bilibili.app.comm.comment2.model.BiliCommentDetail".hookBeforeMethod(mClassLoader, "isShowFloor", hooker = hooker)
     }
 
 }
