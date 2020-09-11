4ab4893ad13acb6205cebc9549c5d5bd819bc4b5
LoveSy <shana@zju.edu.cn>
2020-09-11 12:48:47 +0800

    Small adjustment of save log to file

--- app/src/main/java/me/iacn/biliroaming/XposedInit.kt
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.kt
@@ -18,7 +18,6 @@ import me.iacn.biliroaming.hook.*
 import me.iacn.biliroaming.utils.Log
 import me.iacn.biliroaming.utils.hookBeforeMethod
 import me.iacn.biliroaming.utils.replaceMethod
-import me.iacn.biliroaming.utils.systemContext
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -46,11 +45,9 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
             @Suppress("DEPRECATION")
             when (lpparam.processName) {
                 "tv.danmaku.bili", "com.bilibili.app.blue", "com.bilibili.app.in" -> {
-                    val logProcess = startLog(lpparam.packageName)
-                    if (!sPrefs.getBoolean("save_log", false) &&
-                            !sPrefs.getBoolean("show_hint", true)) {
-                        logProcess?.destroy()
-                        logFile?.delete()
+                    if (sPrefs.getBoolean("save_log", false) ||
+                            sPrefs.getBoolean("show_hint", true)) {
+                        startLog()
                     }
                     Log.d("BiliBili process launched ...")
                     Log.d("Config: ${sPrefs.all}")
@@ -95,14 +92,11 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
         }
     }
 
-    private fun startLog(packageName: String): Process? {
+    private fun startLog(): Process? {
         return try {
-            val packageContext = systemContext.createPackageContext(packageName, 0)
-            val file = File(packageContext.externalCacheDir, "log.txt")
-            file.delete()
-            file.createNewFile()
-            logFile = file
-            val cmd = arrayOf("logcat", "-T", SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.getDefault()).format(Date()), "-f", file.absolutePath)
+            logFile.delete()
+            logFile.createNewFile()
+            val cmd = arrayOf("logcat", "-T", SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.getDefault()).format(Date()), "-f", logFile.absolutePath)
             Runtime.getRuntime().exec(cmd)
         } catch (e: Throwable) {
             Log.e(e)
@@ -117,7 +111,7 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
         val currentContext by lazy { AndroidAppHelper.currentApplication() as Context }
         private val handler by lazy { Handler(Looper.getMainLooper()) }
         private val toast by lazy { Toast.makeText(currentContext, "", Toast.LENGTH_SHORT) }
-        var logFile: File? = null
+        val logFile by lazy { File(currentContext.externalCacheDir, "log.txt") }
         lateinit var modulePath: String
         lateinit var moduleRes: Resources
 
