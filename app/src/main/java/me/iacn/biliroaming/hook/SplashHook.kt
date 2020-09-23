d0af6d35e627c329d4fbad6fa0ab93c9947799a3
LoveSy <shana@zju.edu.cn>
2020-09-23 21:47:47 +0800

    Fix Splash on ver 6.10.0

--- app/src/main/java/me/iacn/biliroaming/hook/SplashHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/SplashHook.kt
@@ -1,12 +1,15 @@
 package me.iacn.biliroaming.hook
 
+import android.app.Activity
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import me.iacn.biliroaming.BiliBiliPackage.Companion.instance
 import me.iacn.biliroaming.XposedInit
-import me.iacn.biliroaming.utils.*
+import me.iacn.biliroaming.utils.Log
+import me.iacn.biliroaming.utils.callMethodAs
+import me.iacn.biliroaming.utils.hookAfterMethod
 import java.io.File
 
 class SplashHook(classLoader: ClassLoader) : BaseHook(classLoader) {
@@ -15,8 +18,11 @@ class SplashHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         Log.d("startHook: Splash")
 
         instance.brandSplashClass?.hookAfterMethod("onViewCreated", View::class.java, Bundle::class.java) { param ->
+            val activity = param.thisObject.callMethodAs<Activity>("getActivity")
+            val view = param.args[0] as View
             if (XposedInit.sPrefs.getBoolean("custom_splash", false)) {
-                val brandSplash = param.thisObject.getObjectFieldAs<ImageView>("a")
+                val brandId = activity.resources.getIdentifier("brand_splash", "id", activity.packageName)
+                val brandSplash = view.findViewById<ImageView>(brandId)
                 val splashImage = File(XposedInit.currentContext.filesDir, SPLASH_IMAGE)
                 if (splashImage.exists())
                     brandSplash.setImageURI(Uri.fromFile(splashImage))
@@ -24,7 +30,8 @@ class SplashHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                     brandSplash.alpha = .0f
             }
             if (XposedInit.sPrefs.getBoolean("custom_splash_logo", false)) {
-                val brandLogo = param.thisObject.getObjectFieldAs<ImageView>("b")
+                val logoId = activity.resources.getIdentifier("brand_logo", "id", activity.packageName)
+                val brandLogo = view.findViewById<ImageView>(logoId)
                 val logoImage = File(XposedInit.currentContext.filesDir, LOGO_IMAGE)
                 if (logoImage.exists())
                     brandLogo.setImageURI(Uri.fromFile(logoImage))
