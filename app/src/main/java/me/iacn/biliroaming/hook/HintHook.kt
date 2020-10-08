8febbe37bddab8e9bea6cbaa2663c60b9ea20290
LoveSy <shana@zju.edu.cn>
2020-10-09 00:58:15 +0800

    Fix hint hook

--- app/src/main/java/me/iacn/biliroaming/hook/HintHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/HintHook.kt
@@ -4,22 +4,22 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
+import me.iacn.biliroaming.BiliBiliPackage.Companion.instance
 import me.iacn.biliroaming.R
 import me.iacn.biliroaming.XposedInit
 import me.iacn.biliroaming.utils.hookAfterMethod
 import me.iacn.biliroaming.utils.setObjectField
-import me.iacn.biliroaming.BiliBiliPackage.Companion.instance
 
 class HintHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
         if (!XposedInit.sPrefs.getBoolean("show_hint", true)) return
         instance.mainActivityClass?.hookAfterMethod("onCreate", Bundle::class.java) { param ->
             AlertDialog.Builder(param.thisObject as Activity).run {
-                val newContext = context.createConfigurationContext(XposedInit.moduleRes.configuration)
+                val newContext = context.applicationContext.createConfigurationContext(XposedInit.moduleRes.configuration)
                 newContext.setObjectField("mResources", XposedInit.moduleRes)
                 setTitle("哔哩漫游使用说明")
                 setView(View.inflate(newContext, R.layout.feature, null))
-                setNegativeButton("知道了") { _, _ -> XposedInit.sPrefs.edit().putBoolean("show_hint", false).apply()}
+                setNegativeButton("知道了") { _, _ -> XposedInit.sPrefs.edit().putBoolean("show_hint", false).apply() }
                 show()
             }
         }
