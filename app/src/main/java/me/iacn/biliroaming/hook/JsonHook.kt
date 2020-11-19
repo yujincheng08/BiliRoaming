b5273bc8e7a8384057635a37bfb5314657099f45
LoveSy <shana@zju.edu.cn>
2020-11-19 22:42:58 +0800

    Refine Name

--- app/src/main/java/me/iacn/biliroaming/hook/JsonHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/JsonHook.kt
@@ -18,7 +18,7 @@ class JsonHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         val eventEntranceClass = "tv.danmaku.bili.ui.main.event.model.EventEntranceModel".findClassOrNull(mClassLoader)
         val cursorListClass = "com.bilibili.app.comm.comment2.model.BiliCommentCursorList".findClassOrNull(mClassLoader)
         val searchRanksClass = "com.bilibili.search.api.SearchRanks".findClass(mClassLoader)
-        val SearchReferralClass = "com.bilibili.search.api.SearchReferral".findClass(mClassLoader)
+        val searchReferralClass = "com.bilibili.search.api.SearchReferral".findClass(mClassLoader)
 
         instance.fastJsonClass?.hookAfterMethod(instance.fastJsonParse(), String::class.java, Type::class.java, Int::class.javaPrimitiveType, "com.alibaba.fastjson.parser.Feature[]") { param ->
             var result = param.result ?: return@hookAfterMethod
@@ -121,7 +121,7 @@ class JsonHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                     result.getObjectFieldAs<MutableList<*>?>("splashList")?.clear()
                     result.getObjectFieldAs<MutableList<*>?>("strategyList")?.clear()
                 }
-                defaultWordClass, defaultKeywordClass, searchRanksClass, SearchReferralClass -> if (sPrefs.getBoolean("purify_search", false) &&
+                defaultWordClass, defaultKeywordClass, searchRanksClass, searchReferralClass -> if (sPrefs.getBoolean("purify_search", false) &&
                         sPrefs.getBoolean("hidden", false)) {
                     result.javaClass.fields.forEach {
                         if (it.type != Int::class.javaPrimitiveType)
