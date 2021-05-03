ffd753c066ebdab0888ad53aae52067d37bb804f
david082321 <david082321@yahoo.com.tw>
2021-05-04 01:37:48 +0800

    Update BangumiSeasonHook.kt

--- app/src/main/java/me/iacn/biliroaming/hook/BangumiSeasonHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/BangumiSeasonHook.kt
@@ -252,11 +252,9 @@ class BangumiSeasonHook(classLoader: ClassLoader) : BaseHook(classLoader) {
             Log.toast("移除广告 x${old - size}")
         }
         body.getObjectField("data")?.getObjectFieldAs<ArrayList<Any>>("items")?.apply {
-            val old = size
             removeAll {
                 "large_cover_v9" in (it.getObjectFieldAs("cardType") ?: "")
             }
-            Log.toast("移除宽推荐 x${old - size}")
         }
     }
 
