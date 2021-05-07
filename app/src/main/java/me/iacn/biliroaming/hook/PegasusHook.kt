7c52bfc4d79b0c617763ebf0f93a3b0766378a2e
LoveSy <shana@zju.edu.cn>
2021-05-07 15:36:46 +0800

    Fix #154

--- app/src/main/java/me/iacn/biliroaming/hook/PegasusHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/PegasusHook.kt
@@ -4,60 +4,31 @@ import me.iacn.biliroaming.BiliBiliPackage.Companion.instance
 import me.iacn.biliroaming.utils.*
 
 class PegasusHook(classLoader: ClassLoader) : BaseHook(classLoader) {
-    override fun startHook() {
-        val switch = sPrefs.getStringSet("customize_home_recommend", emptySet()).orEmpty()
-        if (switch.isEmpty()) return
-        Log.d("startHook: Pegasus")
+    private val filterSet = sPrefs.getStringSet("customize_home_recommend", emptySet()).orEmpty()
 
-        instance.pegasusFeedClass?.hookAfterMethod(instance.pegasusFeed(), instance.okhttpResponseClass) { param ->
-            removeHomeRecommendItems(param.result)
-        }
+    private val filterMap = mapOf(
+            "advertisement" to arrayListOf("ad", "large_cover_v6", "large_cover_v9"),
+            "article" to arrayListOf("article"),
+            "bangumi" to arrayListOf("bangumi", "special"),
+            "picture" to arrayListOf("picture"),
+            "vertical" to arrayListOf("vertical"),
+            "banner" to arrayListOf("banner"),
+            "live" to arrayListOf("live"),
+            "inline" to arrayListOf("inline")
+    )
+
+    private val filter = filterSet.flatMap {
+        filterMap[it].orEmpty()
     }
 
-    /**
-     * 只能这么改
-     * 因为即使是
-     * ```kotlin
-     * val dataClass = body.getObjectField("data").javaClass
-     * val dataJson = body.getObjectField("data").toJson()
-     * body.setObjectField("data", dataClass.fromJson(dataJson)
-     * ```
-     * 也会导致崩溃
-     */
-    private fun removeHomeRecommendItems(body: Any) {
-        val filterSet = sPrefs.getStringSet("customize_home_recommend", emptySet()).orEmpty()
-        val items = body.getObjectField("data")?.getObjectFieldAs<ArrayList<Any>>("items") ?: return
-        if ("advertisement" in filterSet) {
-            items.removeAll {
-                "ad" in (it.getObjectFieldAs("cardGoto") ?: "") ||
-                        "large_cover_v6" in (it.getObjectFieldAs("cardType") ?: "") ||
-                        "large_cover_v9" in (it.getObjectFieldAs("cardType") ?: "")
-            }
-        }
-        if ("article" in filterSet) {
-            items.removeAll {
-                "article" in (it.getObjectFieldAs("cardGoto") ?: "")
-            }
-        }
-        if ("bangumi" in filterSet) {
-            items.removeAll {
-                "bangumi" in (it.getObjectFieldAs("cardGoto") ?: "") ||
-                        "special" in (it.getObjectFieldAs("cardGoto") ?: "")
-            }
-        }
-        if ("picture" in filterSet) {
-            items.removeAll {
-                "picture" in (it.getObjectFieldAs("cardGoto") ?: "")
-            }
-        }
-        if ("vertical" in filterSet) {
-            items.removeAll {
-                "vertical" in (it.getObjectFieldAs("goTo") ?: "")
-            }
-        }
-        if ("banner" in filterSet) {
-            items.removeAll {
-                "banner" in (it.getObjectFieldAs("cardGoto") ?: "")
+    override fun startHook() {
+        if (filter.isEmpty()) return
+        Log.d("startHook: Pegasus")
+        instance.pegasusFeedClass?.hookAfterMethod(instance.pegasusFeed(), instance.okhttpResponseClass) { param ->
+            param.result.getObjectField("data")?.getObjectFieldAs<ArrayList<Any>>("items")?.removeAll {
+                filter.fold(false) { acc, item ->
+                    acc || item in it.getObjectFieldAs<String?>("cardGoto").orEmpty()
+                }
             }
         }
     }
