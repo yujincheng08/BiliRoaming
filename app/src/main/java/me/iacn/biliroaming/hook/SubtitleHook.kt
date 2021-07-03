39403b11428dc6c8ecee24aaad82980e900ecc0d
david082321 <david082321@yahoo.com.tw>
2021-07-03 15:01:11 +0800

    自定义字幕大小（1/3）

--- app/src/main/java/me/iacn/biliroaming/hook/SubtitleHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/SubtitleHook.kt
@@ -2,9 +2,9 @@ package me.iacn.biliroaming.hook
 
 import android.graphics.Rect
 import android.text.SpannableString
+import android.text.style.AbsoluteSizeSpan
 import android.text.style.ForegroundColorSpan
 import android.text.style.LineBackgroundSpan
-import android.text.style.RelativeSizeSpan
 import me.iacn.biliroaming.BiliBiliPackage.Companion.instance
 import me.iacn.biliroaming.utils.*
 import kotlin.math.roundToInt
@@ -50,7 +50,6 @@ class SubtitleHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                 param.args[2] as Int,
                 param.args[3] as Int
             )
-            val subtitle_font_size = sPrefs.getString("subtitle_font_size", "1") + "f"
             (param.thisObject as SpannableString).run {
                 setSpan(
                     ForegroundColorSpan(sPrefs.getInt("subtitle_font_color", 0x7fffffff)),
@@ -59,7 +58,7 @@ class SubtitleHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                     flags
                 )
                 setSpan(
-                    RelativeSizeSpan(subtitle_font_size.toFloat()),
+                    AbsoluteSizeSpan(sPrefs.getInt("subtitle_font_size", 30), true),
                     start,
                     end,
                     flags
