6fb6b274e2e914308335b689ae6d4f04f33e88d3
david082321 <david082321@yahoo.com.tw>
2021-07-10 20:03:57 +0800

    Refine codes

--- app/src/main/java/me/iacn/biliroaming/hook/JsonHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/JsonHook.kt
@@ -78,9 +78,8 @@ class JsonHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                     if (sPrefs.getBoolean("add_channel", false)) {
                         val bottom = data?.getObjectFieldAs<MutableList<Any>>("bottom")
                         val hasChannel = bottom?.fold(false) { acc, it ->
-                            var uri = it.getObjectFieldAs<String>("uri")
-                            if (uri == null) uri = ""
-                            acc || uri.startsWith("bilibili://pegasus/channel")
+                            val uri = it.getObjectFieldAs<String>("uri")
+                            acc || uri?.startsWith("bilibili://pegasus/channel") == true
                         }
                         if (hasChannel != null && !hasChannel) {
                             val channelJson = tabClass?.new()
