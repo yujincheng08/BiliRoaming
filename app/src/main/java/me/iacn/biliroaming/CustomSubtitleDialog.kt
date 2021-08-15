46471bbadf1d76e9591d66d634027e8f443b9402
duzhaokun123 <duzhaokun2@outlook.com>
2021-08-15 17:29:07 +0800

    自定义字幕描边 (#238)
    * 自定义字幕描边
    
    * 避免不必要的绘制

--- app/src/main/java/me/iacn/biliroaming/CustomSubtitleDialog.kt
+++ app/src/main/java/me/iacn/biliroaming/CustomSubtitleDialog.kt
@@ -26,6 +26,10 @@ class CustomSubtitleDialog(val activity: Activity,prefs: SharedPreferences) : Al
         fontSize.setText(prefs.getInt(fontSize.tag.toString(), 30).toString())
         val fontBlurSolid = view.findViewById<EditText>(R.id.font_blur_solid)
         fontBlurSolid.setText(prefs.getInt(fontBlurSolid.tag.toString(), 1).toString())
+        val strokeColor = view.findViewById<EditText>(R.id.strok_color)
+        strokeColor.setText(prefs.getString(strokeColor.tag.toString(), "00000000"))
+        val strokeWidth = view.findViewById<EditText>(R.id.stroke_width)
+        strokeWidth.setText(prefs.getFloat(strokeWidth.tag.toString(), 0F).toString())
         val btnPv = view.findViewById<Button>(R.id.btn_pv)
         btnPv.setOnClickListener {
             val testText = view.findViewById<EditText>(R.id.et_testText).text.toString()
@@ -34,7 +38,9 @@ class CustomSubtitleDialog(val activity: Activity,prefs: SharedPreferences) : Al
             val bc = backgroundColor.text.toString()
             val fs = fontSize.text.toString().toInt()
             val fbs = fontBlurSolid.text.toString().toInt()
-            SubtitleHook.subtitleStylizeRunner(spannableString, 0, spannableString.length, Spanned.SPAN_INCLUSIVE_EXCLUSIVE, fbs, fc, fs, bc)
+            val sc = strokeColor.text.toString()
+            val sw = strokeWidth.text.toString().toFloat()
+            SubtitleHook.subtitleStylizeRunner(spannableString, 0, spannableString.length, Spanned.SPAN_INCLUSIVE_EXCLUSIVE, fbs, fc, fs, bc, sc, sw)
             view.findViewById<TextView>(R.id.tv_pvBlack).text = spannableString
             view.findViewById<TextView>(R.id.tv_pvWhite).text = spannableString
         }
@@ -52,12 +58,23 @@ class CustomSubtitleDialog(val activity: Activity,prefs: SharedPreferences) : Al
                 }
             }.show()
         }
+        view.findViewById<Button>(R.id.btn_chooseColorSc).setOnClickListener {
+            ARGBColorChooseDialog(activity, Color.parseColor("#${strokeColor.text}")).apply {
+                setPositiveButton(android.R.string.ok) {_, _ ->
+                    strokeColor.setText(String.format("%08X", 0xFFFFFFFF.toInt() and color))
+                }
+            }.show()
+        }
 
         setPositiveButton(android.R.string.ok) { _, _ ->
-            prefs.edit().putString(fontColor.tag.toString(), fontColor.text.toString()).apply()
-            prefs.edit().putString(backgroundColor.tag.toString(), backgroundColor.text.toString()).apply()
-            prefs.edit().putInt(fontSize.tag.toString(), fontSize.text.toString().toInt()).apply()
-            prefs.edit().putInt(fontBlurSolid.tag.toString(), fontBlurSolid.text.toString().toInt()).apply()
+            prefs.edit().apply {
+                putString(fontColor.tag.toString(), fontColor.text.toString())
+                putString(backgroundColor.tag.toString(), backgroundColor.text.toString())
+                putInt(fontSize.tag.toString(), fontSize.text.toString().toInt())
+                putInt(fontBlurSolid.tag.toString(), fontBlurSolid.text.toString().toInt())
+                putString(strokeColor.tag.toString(), strokeColor.text.toString())
+                putFloat(strokeWidth.tag.toString(), strokeWidth.text.toString().toFloat())
+            }.apply()
         }
 
         setTitle("自定义字幕样式")
---
+++ app/src/main/java/me/iacn/biliroaming/utils/StrokeSpan.kt
@@ -0,0 +1,47 @@
+package me.iacn.biliroaming.utils
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.text.style.ReplacementSpan
+
+class StrokeSpan(private val fillColor: Int, private val strokeColor: Int, private val strokeWidth: Float) : ReplacementSpan() {
+    private fun fillPaint(paint: Paint): Paint =
+        Paint(paint).apply {
+            style = Paint.Style.FILL
+            color = fillColor
+        }
+
+    private fun stokePaint(paint: Paint): Paint =
+        Paint(paint).apply {
+            style = Paint.Style.STROKE
+            color = strokeColor
+            strokeWidth = this@StrokeSpan.strokeWidth
+        }
+
+    override fun getSize(
+        p0: Paint,
+        p1: CharSequence?,
+        p2: Int,
+        p3: Int,
+        p4: Paint.FontMetricsInt?
+    ): Int {
+        return p0.measureText(p1, p2, p3).toInt()
+    }
+
+    override fun draw(
+        canvas: Canvas,
+        text: CharSequence?,
+        start: Int,
+        end: Int,
+        x: Float,
+        top: Int,
+        y: Int,
+        bottom: Int,
+        paint: Paint
+    ) {
+        text ?: return
+        canvas.drawText(text, start, end, x, y.toFloat(), fillPaint(paint))
+        if (strokeWidth > 0)
+            canvas.drawText(text, start, end, x, y.toFloat(), stokePaint(paint))
+    }
+}
