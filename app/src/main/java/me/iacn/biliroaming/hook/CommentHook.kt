671ebaf5fad51ecddb6d656692d45d9229554c00
littleWhiteDuck <57512745+littleWhiteDuck@users.noreply.github.com>
2021-09-15 22:05:44 +0800

    Update CommentHook.kt

--- app/src/main/java/me/iacn/biliroaming/hook/CommentHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/CommentHook.kt
@@ -1,5 +1,6 @@
 package me.iacn.biliroaming.hook
 
+import android.view.View
 import me.iacn.biliroaming.BiliBiliPackage.Companion.instance
 import me.iacn.biliroaming.utils.replaceMethod
 import me.iacn.biliroaming.utils.sPrefs
@@ -7,8 +8,8 @@ import me.iacn.biliroaming.utils.sPrefs
 class CommentHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
         if (!sPrefs.getBoolean("comment_copy", false)) return
-        instance.commentCopyClass?.replaceMethod( "a", Void::class.java){
-            java.lang.Boolean.TRUE
+        instance.commentCopyClass?.replaceMethod( "onLongClick", View::class.java){
+            true
         }
     }
 }
