995d7fa2a0362f3c07deee3501c62634e8141c1c
LoveSy <shana@zju.edu.cn>
2021-10-22 19:24:26 +0800

    Remove obsolete codes

--- app/src/main/java/me/iacn/biliroaming/BiliBiliPackage.kt
+++ app/src/main/java/me/iacn/biliroaming/BiliBiliPackage.kt
@@ -220,6 +220,7 @@ class BiliBiliPackage constructor(private val mClassLoader: ClassLoader, mContex
     }
 
     val okioWrapperClass by Weak { mHookInfo["class_okio_wrapper"]?.findClassOrNull(mClassLoader) }
+    val progressBarClass by Weak { "tv.danmaku.biliplayer.view.RingProgressBar".findClassOrNull(mClassLoader) ?: "com.bilibili.playerbizcommon.view.RingProgressBar".findClassOrNull(mClassLoader) }
 
     val classesList by lazy {
         mClassLoader.allClassesList {
@@ -533,11 +534,6 @@ class BiliBiliPackage constructor(private val mClassLoader: ClassLoader, mContex
             findGson()
         }.checkConjunctiveOrPut("class_player_options_panel_holder", "field_playback_speed_list") {
             findPlaybackSpeedList()
-        }.checkConjunctiveOrPut(
-            "class_playerparams_bundle",
-            "method_put_serializable_to_playerparams_bundle"
-        ) {
-            findPlayerParamsBundle()
         }.checkConjunctiveOrPut("class_player_core_service_v2", "method_get_default_speed") {
             findGetDefaultSpeed()
         }.checkConjunctiveOrPut("method_gson_tojson", "method_gson_fromjson") {
@@ -683,17 +679,6 @@ class BiliBiliPackage constructor(private val mClassLoader: ClassLoader, mContex
         playerCoreServiceV2class.declaredMethods.forEach { m ->
             if (Modifier.isPublic(m.modifiers) && m.parameterTypes.size == 1 && m.parameterTypes[0] == Boolean::class.java && m.returnType == Float::class.javaPrimitiveType)
                 return arrayOf(playerCoreServiceV2class.name, m.name)
-        }
-        return arrayOfNulls(2)
-    }
-
-    private fun findPlayerParamsBundle(): Array<String?> {
-        val playerParamsBundleClass =
-            "tv.danmaku.biliplayer.basic.context.c".findClassOrNull(mClassLoader)
-                ?: return arrayOfNulls(2)
-        playerParamsBundleClass.declaredMethods.forEach { m ->
-            if (Modifier.isPublic(m.modifiers) && Modifier.isFinal(m.modifiers) && m.parameterTypes.size == 2 && m.parameterTypes[0] == String::class.java && m.parameterTypes[1] == Serializable::class.java)
-                return arrayOf(playerParamsBundleClass.name, m.name)
         }
         return arrayOfNulls(2)
     }
@@ -1034,8 +1019,6 @@ class BiliBiliPackage constructor(private val mClassLoader: ClassLoader, mContex
     }
 
     private fun findPartySectionClass(): String? {
-        val progressBarClass =
-            "tv.danmaku.biliplayer.view.RingProgressBar".findClassOrNull(mClassLoader)
         return classesList.filter {
             it.startsWith("tv.danmaku.bili.ui.video.party.section")
         }.firstOrNull { c ->
@@ -1046,8 +1029,6 @@ class BiliBiliPackage constructor(private val mClassLoader: ClassLoader, mContex
     }
 
     private fun findSectionClass(): String? {
-        val progressBarClass =
-            "tv.danmaku.biliplayer.view.RingProgressBar".findClassOrNull(mClassLoader)
         return classesList.filter {
             it.startsWith("tv.danmaku.bili.ui.video.section")
         }.firstOrNull { c ->
