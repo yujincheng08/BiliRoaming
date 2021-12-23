123c8f670e5a381b840da6e36560384a0ac40c87
vvb2060 <vvb2060@gmail.com>
2021-12-23 17:25:03 +0800

    Fix customize accessKey
    Co-authored-by: Jason_Khew <29531167+JasonKhew96@users.noreply.github.com>

--- app/src/main/java/me/iacn/biliroaming/XposedInit.kt
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.kt
@@ -86,6 +86,7 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
                     startHook(CommentHook(lpparam.classLoader))
                     startHook(LiveRoomHook(lpparam.classLoader))
                     startHook(RecommendHook(lpparam.classLoader))
+                    startHook(QualityHook(lpparam.classLoader))
                 }
                 lpparam.processName.endsWith(":web") -> {
                     BiliBiliPackage(lpparam.classLoader, param.args[0] as Context)
