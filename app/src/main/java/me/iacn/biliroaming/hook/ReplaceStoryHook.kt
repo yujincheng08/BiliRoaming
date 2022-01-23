a54ee7b8c0d1ca370b780e5e21f3c3ec48cc1ce1
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2022-01-23 22:17:40 +0800

    Replace story video with normal video

--- app/src/main/java/me/iacn/biliroaming/hook/ReplaceStoryHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/ReplaceStoryHook.kt
@@ -1,7 +1,6 @@
 package me.iacn.biliroaming.hook
 
 import android.app.Activity
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import me.iacn.biliroaming.utils.sPrefs
@@ -12,10 +11,6 @@ import me.iacn.biliroaming.utils.hookBeforeMethod
 class ReplaceStoryHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
         if (!sPrefs.getBoolean("replace_story_video", false)) return;
-        if (instance.storyVideoActivityClass == null) {
-            Log.e("storyVideoActivityClass not found");
-        }
-        Log.d("storyVideoActivityClass=${instance.storyVideoActivityClass}")
         instance.storyVideoActivityClass?.hookBeforeMethod(
             "onCreate",
             Bundle::class.java
@@ -23,18 +18,17 @@ class ReplaceStoryHook(classLoader: ClassLoader) : BaseHook(classLoader) {
             val thiz = param.thisObject as Activity
             val intent = thiz.intent
             val url = intent.data
-            var avid: String? = null
-            if (url != null && url.scheme == "bilibili" && url.host == "story" && url.path?.matches(Regex("/\\d+")) == true) {
-                url.path!!.substring(1).also { avid = it }
-            }
+            val avid: String? = if (url != null && url.scheme == "bilibili" && url.host == "story" && url.path?.matches(Regex("/\\d+")) == true) {
+                url.path!!.substring(1)
+            } else null
             if (avid == null) {
                 Log.toast("解析失败，无法替换竖版视频")
                 return@hookBeforeMethod
             }
-            val newIntent = Intent()
-            newIntent.setPackage(thiz.packageName)
-            newIntent.data = Uri.parse("bilibili://video/${avid}")
-            thiz.startActivity(newIntent)
+            intent.component = null
+            intent.setPackage(thiz.packageName)
+            intent.data = Uri.parse("bilibili://video/${avid}")
+            thiz.startActivity(intent)
             Log.toast("已替换竖版视频")
             thiz.finish()
         }
