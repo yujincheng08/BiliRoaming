d1eeccfb6fbb8da632e39cfc13ac47d367b3aa87
david082321 <7046617+david082321@users.noreply.github.com>
2022-02-02 09:06:30 +0800

    添加注释

--- app/src/main/java/me/iacn/biliroaming/hook/SkinHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/SkinHook.kt
@@ -6,7 +6,13 @@ import org.json.JSONObject
 import java.io.File
 import java.lang.reflect.Array as RArray
 
+/**
+ * Created by david on 2022/2/1
+ * Email ？
+ */
+
 class SkinHook (classLoader: ClassLoader) : BaseHook(classLoader) {
+    // （这里开始）直接抄 BangumiSeasonHook 的内容
     companion object {
         private val jsonNonStrict = lazy {
             instance.kotlinJsonClass?.getStaticObjectField("Companion")?.callMethod("getNonstrict")
@@ -66,23 +72,28 @@ class SkinHook (classLoader: ClassLoader) : BaseHook(classLoader) {
     }
 
     private fun Class<*>.fromJson(json: JSONObject) = fromJson(json.toString())
+    // （这里结束）直接抄 BangumiSeasonHook 的内容
 
     override fun startHook() {
+        // 判断设置打开了吗？
         if (!sPrefs.getBoolean("skin_import", false) && !sPrefs.getBoolean("skin", false)) return
         Log.d("startHook: Skin")
+        // 这里也是直接抄 BangumiSeasonHook 的内容
         instance.retrofitResponseClass?.hookBeforeAllConstructors { param ->
-            val url = getUrl(param.args[0])
+            val url = getUrl(param.args[0]) ?: return@hookBeforeAllConstructors
             val body = param.args[1] ?: return@hookBeforeAllConstructors
             if (instance.generalResponseClass?.isInstance(body) == true ||
                 instance.rxGeneralResponseClass?.isInstance(body) == true
             ) {
-                url ?: return@hookBeforeAllConstructors
+                // 判断网址要不要处理
                 if (url.startsWith("https://app.bilibili.com/x/resource/show/skin?")) {
                     val dataField =
                         if (instance.generalResponseClass?.isInstance(body) == true) "data" else instance.responseDataField().value
                     val resultClass = body.getObjectField(dataField)?.javaClass
                     try {
+                        // 获取用户的json内容
                         val skin = when {
+                            // 从 导入 获取
                             sPrefs.getBoolean("skin_import", false) -> {
                                 File(currentContext.filesDir, "../files/skin.json")
                                     .readText().toJSONObject().putOpt("package_md5","")
@@ -90,18 +101,22 @@ class SkinHook (classLoader: ClassLoader) : BaseHook(classLoader) {
                                         """package_md5":""""", """package_md5":null"""
                                     )
                             }
+                            // 从 填写 获取
                             sPrefs.getBoolean("skin", false) -> {
                                 sPrefs.getString("skin_json", "")?.toJSONObject().toString()
                             }
+                            // 什么都不做(虽然不会有这种可能，但是前面的 when 需要这个)
                             else -> return@hookBeforeAllConstructors
                         }
+                        // 准备替换内容
                         val content = """{"user_equip":""" + skin +
                                 ""","skin_colors":[{"id":8,"name":"简洁白","is_free":true,"color_name":"white"},{"id":2,"name":"少女粉","is_free":true,"color_name":"pink"},{"id":1,"name":"主题黑","is_free":true,"color_name":"black"},{"id":3,"name":"高能红","is_free":true,"color_name":"red"},{"id":4,"name":"咸蛋黄","is_free":true,"color_name":"yellow"},{"id":5,"name":"早苗绿","is_free":true,"color_name":"green"},{"id":6,"name":"宝石蓝","is_free":true,"color_name":"blue"},{"id":7,"name":"罗兰紫","is_free":true,"color_name":"purple"}]}"""
                         val newResult = resultClass?.fromJson(content)
+                        // 替换内容
                         body.setObjectField(dataField, newResult)
                     } catch (e: Exception) {
                         e.printStackTrace()
-                        Log.toast(e.message ?: "发生错误，请关闭自制主题设置", true)
+                        Log.toast("发生错误，请关闭自制主题设置", true)
                     }
                 }
             }
