31922d9497f15372d63f80fa859cdd29ae33c1bf
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-05-05 02:25:27 +0800

    fix: #586 cause crash (#589)

--- app/src/main/java/me/iacn/biliroaming/hook/BangumiSeasonHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/BangumiSeasonHook.kt
@@ -255,9 +255,7 @@ class BangumiSeasonHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                 ) {
                     fixBangumi(body, url)
                 }
-                if (url != null && (url.startsWith("https://appintl.biliapi.net/intl/gateway/app/search/type") || url.startsWith(
-                        "https://app.bilibili.com/x/intl/search/type"
-                    ))
+                if (url != null && url.startsWith("https://appintl.biliapi.net/intl/gateway/app/search/type")
                 ) {
                     val area = Uri.parse(url).getQueryParameter("type")?.toInt()
                     if (!AREA_TYPES.containsKey(area)) {
