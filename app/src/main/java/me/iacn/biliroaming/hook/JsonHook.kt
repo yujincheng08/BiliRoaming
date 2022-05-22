535a9297ebc1c7a56e042ff8f6386424cba34e3b
LoveSy <shana@zju.edu.cn>
2022-05-23 03:07:43 +0800

    Fix NPE

--- app/src/main/java/me/iacn/biliroaming/hook/JsonHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/JsonHook.kt
@@ -385,8 +385,8 @@ class JsonHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                 splashClass -> if (sPrefs.getBoolean("purify_splash", false) &&
                     sPrefs.getBoolean("hidden", false)
                 ) {
-                    result.getObjectFieldAs<MutableList<*>?>("splashList")?.clear()
-                    result.getObjectFieldAs<MutableList<*>?>("strategyList")?.clear()
+                    result.getObjectFieldOrNullAs<MutableList<*>>("splashList")?.clear()
+                    result.getObjectFieldOrNullAs<MutableList<*>>("strategyList")?.clear()
                 }
                 defaultWordClass, defaultKeywordClass, searchRanksClass, searchReferralClass, followingcardSearchRanksClass -> if (sPrefs.getBoolean(
                         "purify_search",
@@ -402,10 +402,8 @@ class JsonHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                 brandSplashDataClass -> if (sPrefs.getBoolean("custom_splash", false) ||
                     sPrefs.getBoolean("custom_splash_logo", false)
                 ) {
-                    val brandList = result.getObjectFieldAs<MutableList<Any>>("brandList")
-                    val showList = result.getObjectFieldAs<MutableList<Any>>("showList")
-                    brandList.clear()
-                    showList.clear()
+                    result.getObjectFieldOrNullAs<MutableList<Any>>("brandList")?.clear()
+                    result.getObjectFieldOrNullAs<MutableList<Any>>("showList")?.clear()
                 }
                 eventEntranceClass -> if (sPrefs.getBoolean("purify_game", false) &&
                     sPrefs.getBoolean("hidden", false)
