38334fabbff62d602a65273d63efc1af35a9cdfa
iamr0s <271257581@qq.com>
2022-08-05 21:34:06 +0800

    fix replace story video

--- app/src/main/java/me/iacn/biliroaming/hook/ReplaceStoryHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/ReplaceStoryHook.kt
@@ -4,35 +4,19 @@ import android.app.Activity
 import android.net.Uri
 import android.os.Bundle
 import me.iacn.biliroaming.BiliBiliPackage.Companion.instance
-import me.iacn.biliroaming.utils.Log
-import me.iacn.biliroaming.utils.hookBeforeMethod
-import me.iacn.biliroaming.utils.sPrefs
+import me.iacn.biliroaming.utils.*
 
 class ReplaceStoryHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
         if (!sPrefs.getBoolean("replace_story_video", false)) return
-        instance.storyVideoActivityClass?.hookBeforeMethod(
-            "onCreate",
-            Bundle::class.java
+        instance.basicIndexItemClass?.hookAfterMethod(
+            "getUri"
         ) { param ->
-            val thiz = param.thisObject as Activity
-            val intent = thiz.intent
-            val url = intent.data
-            if (url != null && url.scheme == "bilibili" && url.host?.startsWith("story") == true && url.path?.matches(
-                    Regex("/\\d+")
-                ) == true
-            ) {
-                intent.data = Uri.parse("bilibili://video/${url.path?.substring(1)}")
-            } else {
-                Log.toast("解析失败，无法替换竖版视频")
-                Log.e("parse $url as story failed")
-                return@hookBeforeMethod
+            var uri = (param.result ?: return@hookAfterMethod) as String
+            if (uri.startsWith("bilibili://story/")) {
+                uri = "bilibili://video/" + uri.substringAfter("bilibili://story/")
             }
-            intent.component = null
-            intent.setPackage(thiz.packageName)
-            thiz.startActivity(intent)
-            Log.toast("已替换竖版视频")
-            thiz.finish()
+            param.result = uri
         }
     }
 }
