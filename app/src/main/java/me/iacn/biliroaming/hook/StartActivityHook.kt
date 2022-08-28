c265a3a4ad8d278700c34c507704a8aab7a05efa
LoveSy <shana@zju.edu.cn>
2022-08-28 12:20:58 +0800

    Format codes

--- app/src/main/java/me/iacn/biliroaming/hook/StartActivityHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/StartActivityHook.kt
@@ -1,11 +1,9 @@
 package me.iacn.biliroaming.hook
 
-import android.app.Activity
 import android.app.Instrumentation
 import android.content.ComponentName
 import android.content.Intent
 import android.net.Uri
-import me.iacn.biliroaming.utils.Log
 import me.iacn.biliroaming.utils.hookBeforeAllMethods
 import me.iacn.biliroaming.utils.packageName
 import me.iacn.biliroaming.utils.sPrefs
@@ -15,12 +13,17 @@ class StartActivityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         Instrumentation::class.java.hookBeforeAllMethods("execStartActivity") { param ->
             val intent = param.args[4] as? Intent ?: return@hookBeforeAllMethods
             val uri = intent.dataString ?: return@hookBeforeAllMethods
-            if (sPrefs.getBoolean("replace_story_video", false) && uri.startsWith("bilibili://story/")) {
+            if (sPrefs.getBoolean(
+                    "replace_story_video",
+                    false
+                ) && uri.startsWith("bilibili://story/")
+            ) {
                 intent.component = ComponentName(
                     intent.component?.packageName ?: packageName,
                     "com.bilibili.video.videodetail.VideoDetailsActivity"
                 )
-                intent.data = Uri.parse(intent.dataString?.replace("bilibili://story/", "bilibili://video/"))
+                intent.data =
+                    Uri.parse(intent.dataString?.replace("bilibili://story/", "bilibili://video/"))
             }
             if (sPrefs.getBoolean("force_browser", false)) {
                 if (intent.component?.className?.endsWith("MWebActivity") == true) {
@@ -29,7 +32,6 @@ class StartActivityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                     }
                 }
             }
-
         }
     }
 }
