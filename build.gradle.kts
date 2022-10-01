7dc9f6b4a13b274dc550d10f180423841fdcd90b
LoveSy <shana@zju.edu.cn>
2022-10-01 15:08:50 +0800

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion by extra("1.7.0")
-    val protobufVersion by extra("3.21.6")
+    val kotlinVersion by extra("1.7.20")
+    val protobufVersion by extra("3.21.7")
     repositories {
         google()
         mavenCentral()
@@ -10,7 +10,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.18")
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.19")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
