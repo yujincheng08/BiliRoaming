418bcf3af5cd4fe6b64132d1d1205dff8a6419dd
cecilia-little1 <113076143+cecilia-little1@users.noreply.github.com>
2022-10-19 17:59:35 +0800

    Allow to disable player long click to accelerate (#828)
    Co-authored-by: LoveSy <shana@zju.edu.cn>

---
+++ app/src/main/java/me/iacn/biliroaming/hook/PlayerLongPressHook.kt
@@ -0,0 +1,31 @@
+package me.iacn.biliroaming.hook
+
+import android.view.MotionEvent
+import me.iacn.biliroaming.utils.Hooker
+import me.iacn.biliroaming.utils.Log
+import me.iacn.biliroaming.utils.hookBeforeMethod
+import me.iacn.biliroaming.utils.sPrefs
+
+class PlayerLongPressHook(classLoader: ClassLoader) : BaseHook(classLoader) {
+    override fun startHook() {
+        if (!sPrefs.getBoolean("forbid_player_long_click_accelerate", false)) return
+
+        Log.d("startHook: PlayerLongPress");
+
+        val hooker: Hooker = { param -> param.result = true }
+        // pre 6.59.0
+        "tv.danmaku.biliplayerimpl.gesture.GestureService\$mTouchListener\$1".hookBeforeMethod(
+            mClassLoader,
+            "onLongPress",
+            MotionEvent::class.java,
+            hooker = hooker
+        )
+        // post 6.59.0
+        arrayOf(
+            "tv.danmaku.biliplayerimpl.gesture.GestureService\$initInnerLongPressListener\$1\$onLongPress\$1",
+            "tv.danmaku.biliplayerimpl.gesture.GestureService\$initInnerLongPressListener\$1\$onLongPressEnd\$1"
+        ).forEach { className ->
+            className.hookBeforeMethod(mClassLoader, "invoke", Object::class.java, hooker = hooker)
+        }
+    }
+}
