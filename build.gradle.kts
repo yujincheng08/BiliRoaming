a532cc339e0907210a144b549c244ecd510a5036
LoveSy <shana@zju.edu.cn>
2022-12-20 09:28:19 +0800

    [skip ci] Bump 1.6.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.eclipse.jgit.internal.storage.file.FileRepository
 
 buildscript {
     val kotlinVersion by extra("1.7.21")
-    val protobufVersion by extra("3.21.10")
+    val protobufVersion by extra("3.21.12")
     repositories {
         google()
         mavenCentral()
