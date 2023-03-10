ac3f237169b6bc05a5c4e0a8605b75758609f790
Kofua <1638183271zjn@gmail.com>
2023-03-11 07:47:10 +0800

    Implement `saveImage` bridge api
    Fix #974

--- app/src/main/java/me/iacn/biliroaming/hook/CommentImageHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/CommentImageHook.kt
@@ -16,6 +16,63 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class CommentImageHook(classLoader: ClassLoader) : BaseHook(classLoader) {
+    companion object {
+        fun saveImage(url: String) {
+            runCatching {
+                URL(url).openStream().use { stream ->
+                    val relativePath =
+                        "${Environment.DIRECTORY_PICTURES}${File.separator}bilibili"
+                    val fullFilename = url.substringAfterLast('/')
+                    val filename = fullFilename.substringBeforeLast('.')
+
+                    val now = System.currentTimeMillis()
+                    val contentValues = ContentValues().apply {
+                        put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                        put(
+                            MediaStore.MediaColumns.MIME_TYPE,
+                            HttpURLConnection.guessContentTypeFromName(fullFilename)
+                                ?: "image/png"
+                        )
+                        put(MediaStore.MediaColumns.DATE_ADDED, now / 1000)
+                        put(MediaStore.MediaColumns.DATE_MODIFIED, now / 1000)
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            put(MediaStore.MediaColumns.DATE_TAKEN, now)
+                            put(MediaStore.MediaColumns.RELATIVE_PATH, relativePath)
+                        } else {
+                            val path = File(
+                                Environment.getExternalStoragePublicDirectory(
+                                    Environment.DIRECTORY_PICTURES
+                                ), "bilibili"
+                            ).also { it.mkdirs() }
+                            put(
+                                MediaStore.MediaColumns.DATA,
+                                File(path, fullFilename).absolutePath
+                            )
+                        }
+                    }
+                    val resolver = currentContext.contentResolver
+                    val uri = resolver.insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+                    )
+                    runCatching {
+                        resolver.openOutputStream(uri!!)?.use { stream.copyTo(it) }
+                    }.onSuccess {
+                        Log.toast(
+                            "图片已保存至\n$relativePath${File.separator}$fullFilename",
+                            true
+                        )
+                    }.onFailure {
+                        Log.e(it)
+                        Log.toast("图片保存失败，可能已经保存或未授予权限", true)
+                    }
+                }
+            }.onFailure {
+                Log.e(it)
+                Log.toast("图片获取失败", force = true)
+            }
+        }
+    }
+
     @Suppress("UNCHECKED_CAST")
     override fun startHook() {
         if (!sPrefs.getBoolean("save_comment_image", false)) return
@@ -29,58 +86,7 @@ class CommentImageHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                 imageUrl ?: continue
                 image.setOnLongClickListener {
                     MainScope().launch(Dispatchers.IO) {
-                        runCatching {
-                            URL(imageUrl).openStream().use { stream ->
-                                val relativePath =
-                                    "${Environment.DIRECTORY_PICTURES}${File.separator}bilibili"
-                                val fullFilename = imageUrl.substringAfterLast('/')
-                                val filename = fullFilename.substringBeforeLast('.')
-
-                                val now = System.currentTimeMillis()
-                                val contentValues = ContentValues().apply {
-                                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
-                                    put(
-                                        MediaStore.MediaColumns.MIME_TYPE,
-                                        HttpURLConnection.guessContentTypeFromName(fullFilename)
-                                            ?: "image/png"
-                                    )
-                                    put(MediaStore.MediaColumns.DATE_ADDED, now / 1000)
-                                    put(MediaStore.MediaColumns.DATE_MODIFIED, now / 1000)
-                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                                        put(MediaStore.MediaColumns.DATE_TAKEN, now)
-                                        put(MediaStore.MediaColumns.RELATIVE_PATH, relativePath)
-                                    } else {
-                                        val path = File(
-                                            Environment.getExternalStoragePublicDirectory(
-                                                Environment.DIRECTORY_PICTURES
-                                            ), "bilibili"
-                                        ).also { it.mkdirs() }
-                                        put(
-                                            MediaStore.MediaColumns.DATA,
-                                            File(path, fullFilename).absolutePath
-                                        )
-                                    }
-                                }
-                                val resolver = currentContext.contentResolver
-                                val uri = resolver.insert(
-                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
-                                )
-                                runCatching {
-                                    resolver.openOutputStream(uri!!)?.use { stream.copyTo(it) }
-                                }.onSuccess {
-                                    Log.toast(
-                                        "图片已保存至\n$relativePath${File.separator}$fullFilename",
-                                        true
-                                    )
-                                }.onFailure {
-                                    Log.e(it)
-                                    Log.toast("图片保存失败，可能已经保存或未授予权限", true)
-                                }
-                            }
-                        }.onFailure {
-                            Log.e(it)
-                            Log.toast("图片获取失败", force = true)
-                        }
+                        saveImage(imageUrl)
                     }
                     true
                 }
