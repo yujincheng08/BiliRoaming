b36a765d6684b1b6c13199c6654ab7ad7a6a7a2c
cxw620 <cxwdyx620@gmail.com>
2023-03-24 23:59:11 +0800

    remove debug log

--- app/src/main/java/me/iacn/biliroaming/hook/P2pHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/P2pHook.kt
@@ -12,60 +12,30 @@ class P2pHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         val blockPcdnLive = sPrefs.getBoolean("block_pcdn_live", false)
         if (blockPcdn) {
             "tv.danmaku.ijk.media.player.P2P".from(mClassLoader)?.run {
-                replaceMethod("resolveP2PServerUrls", Bundle::class.java) {
-                    Log.d("block pcdn -> tv.danmaku.ijk.media.player.P2P#resolveP2PServerUrls")
-                }
-                replaceMethod("isNeedCreateClient") { 
-                    Log.d("block pcdn -> tv.danmaku.ijk.media.player.P2P#isNeedCreateClient")
-                    false
-                }
-                replaceMethod("getHttpServerPort") { 
-                    Log.d("block pcdn -> tv.danmaku.ijk.media.player.P2P#getHttpServerPort")
-                    -1
-                }
-                replaceMethod("isServerEffective", List::class.java, List::class.java) {
-                    Log.d("block pcdn -> tv.danmaku.ijk.media.player.P2P#isServerEffective")
-                    false
-                }
+                replaceMethod("resolveP2PServerUrls", Bundle::class.java) {}
+                replaceMethod("isNeedCreateClient") { false }
+                replaceMethod("getHttpServerPort") { -1 }
+                replaceMethod("isServerEffective", List::class.java, List::class.java) { false }
             }
         }
         if (blockPcdnLive) {
-            "com.bilibili.sistersplayer.p2p.P2PContext".from(mClassLoader)?.run {
-                replaceMethod("setConfiguration", String::class.java) {
-                    Log.d("block pcdn -> com.bilibili.sistersplayer.p2p.P2PContext#setConfiguration")
-                }
-            }
+            "com.bilibili.sistersplayer.p2p.P2PContext".from(mClassLoader)?.replaceMethod("setConfiguration", String::class.java) {}
             "com.bilibili.sistersplayer.p2p.SistersPlayerLoader".from(mClassLoader)?.run {
                 hookBeforeMethod("setCanP2PUpload", Boolean::class.java) { param ->
-                    Log.d("block pcdn -> com.bilibili.sistersplayer.p2p.SistersPlayerLoader#setCanP2PUpload RAW ${param.args[0]} -> false")
                     param.args[0] = false
                 }
-                replaceMethod("initP2PContext") {
-                    Log.d("block pcdn -> com.bilibili.sistersplayer.p2p.SistersPlayerLoader#initP2PContext")
-                }
+                replaceMethod("initP2PContext") {}
             }
             "com.bilibili.sistersplayer.p2p.peer.NyaPeer".from(mClassLoader)?.run {
-                replaceMethod("shouldUsedAsSeed") {
-                    Log.d("block pcdn -> com.bilibili.sistersplayer.p2p.peer.NyaPeer#shouldUsedAsSeed")
-                    false
-                }
-                replaceMethod("acceptAnswer", String::class.java) {
-                    Log.d("block pcdn -> com.bilibili.sistersplayer.p2p.peer.NyaPeer#acceptAnswer")
-                }
+                replaceMethod("shouldUsedAsSeed") { false }
+                replaceMethod("acceptAnswer", String::class.java) {}
             }
             "com.bilibili.sistersplayer.p2p.tracker.GetPeersResult".from(mClassLoader)?.run {
-                replaceMethod("getPeers") {
-                    Log.d("block pcdn -> com.bilibili.sistersplayer.p2p.tracker.GetPeersResult#getPeers")
-                }
-                replaceMethod("getLeeches") {
-                    Log.d("block pcdn -> com.bilibili.sistersplayer.p2p.tracker.GetPeersResult#getLeeches")
-                }
+                replaceMethod("getPeers") {}
+                replaceMethod("getLeeches") {}
             }
             // optimize non p2p perf
-            instance.liveNetworkTypeClass?.replaceMethod(instance.liveNetworkType()) {
-                Log.d("block pcdn -> liveNetworkTypeClass#liveNetworkType()")
-                "unknown"
-            }
+            instance.liveNetworkTypeClass?.replaceMethod(instance.liveNetworkType()) { "unknown" }
         }
     }
 }
