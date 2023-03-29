a81192f7fbab9827d19c2b761c143e6877f9509a
cxw620 <cxwdyx620@gmail.com>
2023-03-30 00:46:05 +0800

    feat: allow disable MainPageStory

--- app/src/main/java/me/iacn/biliroaming/hook/MainPageStoryHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/MainPageStoryHook.kt
@@ -5,14 +5,15 @@ import me.iacn.biliroaming.utils.*
 class MainPageStoryHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
         Log.d("startHook: MainPageStory")
-        "com.bapis.bilibili.app.distribution.setting.experimental.TopLeft".from(mClassLoader)?.run {
-            hookAfterMethod("getUrl") { param ->
-                Log.d("com.bapis.bilibili.app.distribution.setting.experimental.TopLeft#getUrl -> hooked")
-                param.result?.callMethod("setValue", "bilibili://root?tab_name=我的")
-            }
-            hookAfterMethod("getUrlV2") { param ->
-                Log.d("com.bapis.bilibili.app.distribution.setting.experimental.TopLeft#getUrlV2 -> hooked")
-                param.result?.callMethod("setValue", "bilibili://root?tab_name=我的")
+        val disableMainPageStory = sPrefs.getBoolean("disable_main_page_story", false)
+        if (disableMainPageStory) {
+            "com.bapis.bilibili.app.distribution.setting.experimental.TopLeft".from(mClassLoader)?.run {
+                hookAfterMethod("getUrl") { param ->
+                    param.result?.callMethod("setValue", "bilibili://root?tab_name=我的")
+                }
+                hookAfterMethod("getUrlV2") { param ->
+                    param.result?.callMethod("setValue", "bilibili://root?tab_name=我的")
+                }
             }
         }
     }
