ea40ea6bfe4bd39b6185eb6c28294db2e59e74b8
cxw620 <cxwdyx620@gmail.com>
2023-03-30 13:02:50 +0800

    typo

--- app/src/main/java/me/iacn/biliroaming/hook/MainPageStoryHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/MainPageStoryHook.kt
@@ -5,8 +5,7 @@ import me.iacn.biliroaming.utils.*
 class MainPageStoryHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
         Log.d("startHook: MainPageStory")
-        val disableMainPageStory = sPrefs.getBoolean("disable_main_page_story", false)
-        if (disableMainPageStory) {
+        if (sPrefs.getBoolean("disable_main_page_story", false)) {
             "com.bapis.bilibili.app.distribution.setting.experimental.TopLeft".from(mClassLoader)?.run {
                 hookAfterMethod("getUrl") { param ->
                     param.result?.callMethod("setValue", "bilibili://root?tab_name=我的")
