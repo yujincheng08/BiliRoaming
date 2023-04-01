c3ac04b160be8b632a51df46ecda22d15a3723ce
cxw620 <cxwdyx620@gmail.com>
2023-04-01 19:18:47 +0800

    feat: BangumiPageAdHook dev

--- app/src/main/java/me/iacn/biliroaming/hook/BangumiPageAdHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/BangumiPageAdHook.kt
@@ -7,36 +7,32 @@ import me.iacn.biliroaming.utils.*
 
 class BangumiPageAdHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
-        Log.d("startHook: BangumiPageAd")
-        // toast activity
-        "com.bilibili.bangumi.data.page.detail.entity.OGVActivityVo".from(mClassLoader)?.hookBeforeConstructor(
-            Int::class.javaPrimitiveType, 
-            "com.bilibili.bangumi.data.page.detail.entity.OGVActivityBarVo", 
-            "com.bilibili.bangumi.data.page.detail.entity.OGVInvitationActivityHostVo",
-            "com.bilibili.bangumi.data.page.detail.entity.OGVInvitationActivityGuestVo",
-            List::class.java,
-            "com.bilibili.bangumi.data.page.detail.entity.VipWatchingCountdownTaskVo",
-            "com.bilibili.bangumi.data.page.detail.entity.OGVActivityDialogVo"
-        ) { param ->
-            Log.d("OGVActivity hook successfully.")
-            Log.d("OGVActivity Details: ${param.args[0]}, ${param.args[1]}, ${param.args[2]}, ${param.args[3]}, ${param.args[4]}, ${param.args[5]}, ${param.args[6]}")
-            param.args[0] = 0
-            param.args[1] = null
-            param.args[2] = null
-            param.args[3] = null
-            param.args[4].callMethodAs<List<*>?>("clear")
-            param.args[5] = null
-            param.args[6] = null
-        }
-        // mall
-        "com.bilibili.bangumi.data.page.detail.entity.BangumiUniformSeason".from(mClassLoader)?.let { class_ ->
-            val activityEntranceListMethod = class_.declaredMethods?.find {
-                it.parameterTypes.size == 0 && it.genericReturnType.toString().contains("ActivityEntrance")
-            } ?.name
-            Log.d("activityEntranceListMethod: $activityEntranceListMethod")
-            class_.replaceMethod(activityEntranceListMethod) {
-                Log.d("activityEntranceListMethod hook successfully.")
-                null
+        if (sPrefs.getBoolean("block_bangumi_page_ads", false)) {
+            Log.d("startHook: BangumiPageAd")
+            // activity toast ad
+            "com.bilibili.bangumi.data.page.detail.entity.OGVActivityVo".from(mClassLoader)?.hookBeforeConstructor(
+                Int::class.javaPrimitiveType, 
+                "com.bilibili.bangumi.data.page.detail.entity.OGVActivityBarVo", 
+                "com.bilibili.bangumi.data.page.detail.entity.OGVInvitationActivityHostVo",
+                "com.bilibili.bangumi.data.page.detail.entity.OGVInvitationActivityGuestVo",
+                List::class.java,
+                "com.bilibili.bangumi.data.page.detail.entity.VipWatchingCountdownTaskVo",
+                "com.bilibili.bangumi.data.page.detail.entity.OGVActivityDialogVo"
+            ) { param ->
+                param.args[0] = 0
+                param.args[1] = null
+                param.args[2] = null
+                param.args[3] = null
+                param.args[4].callMethodAs<List<*>?>("clear")
+                param.args[5] = null
+                param.args[6] = null
+            }
+            // mall
+            "com.bilibili.bangumi.data.page.detail.entity.BangumiUniformSeason".from(mClassLoader)?.let { class_ ->
+                val activityEntranceListMethod = class_.declaredMethods?.find {
+                    it.parameterTypes.size == 0 && it.genericReturnType.toString().contains("ActivityEntrance")
+                } ?.name
+                class_.replaceMethod(activityEntranceListMethod) { null }
             }
         }
     }
