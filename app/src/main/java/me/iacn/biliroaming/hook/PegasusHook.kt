bcde5e546d4266fe77e48ca959a09ea7179801ab
Kofua <1638183271zjn@gmail.com>
2023-05-06 10:25:16 +0800

    Refine codes

--- app/src/main/java/me/iacn/biliroaming/hook/PegasusHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/PegasusHook.kt
@@ -6,31 +6,21 @@ import me.iacn.biliroaming.utils.*
 class PegasusHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     private val filterSet = sPrefs.getStringSet("customize_home_recommend", emptySet()).orEmpty()
 
-    private val hideLowPlayCountLimit by lazy {
-        sPrefs.getLong("hide_low_play_count_recommend_limit", 0)
-    }
-    private val hideShortDurationLimit by lazy {
-        sPrefs.getInt("hide_short_duration_recommend_limit", 0)
-    }
-    private val hideLongDurationLimit by lazy {
-        sPrefs.getInt("hide_long_duration_recommend_limit", 0)
-    }
+    private val hideLowPlayCountLimit = sPrefs.getLong("hide_low_play_count_recommend_limit", 0)
+    private val hideShortDurationLimit = sPrefs.getInt("hide_short_duration_recommend_limit", 0)
+    private val hideLongDurationLimit = sPrefs.getInt("hide_long_duration_recommend_limit", 0)
     private val kwdFilterTitleList by lazy {
         migrateHomeFilterPrefsIfNeeded()
         sPrefs.getStringSet("home_filter_keywords_title", null).orEmpty()
     }
     private val kwdFilterTitleRegexes by lazy { kwdFilterTitleList.map { it.toRegex() } }
-    private val kwdFilterTitleRegexMode by lazy {
-        sPrefs.getBoolean("home_filter_title_regex_mode", false)
-    }
+    private val kwdFilterTitleRegexMode = sPrefs.getBoolean("home_filter_title_regex_mode", false)
     private val kwdFilterReasonList by lazy {
         migrateHomeFilterPrefsIfNeeded()
         sPrefs.getStringSet("home_filter_keywords_reason", null).orEmpty()
     }
     private val kwdFilterReasonRegexes by lazy { kwdFilterReasonList.map { it.toRegex() } }
-    private val kwdFilterReasonRegexMode by lazy {
-        sPrefs.getBoolean("home_filter_reason_regex_mode", false)
-    }
+    private val kwdFilterReasonRegexMode = sPrefs.getBoolean("home_filter_reason_regex_mode", false)
     private val kwdFilterUidList by lazy {
         migrateHomeFilterPrefsIfNeeded()
         sPrefs.getStringSet("home_filter_keywords_uid", null)
@@ -41,9 +31,7 @@ class PegasusHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         sPrefs.getStringSet("home_filter_keywords_up", null).orEmpty()
     }
     private val kwdFilterUpnameRegexes by lazy { kwdFilterUpnameList.map { it.toRegex() } }
-    private val kwdFilterUpnameRegexMode by lazy {
-        sPrefs.getBoolean("home_filter_up_regex_mode", false)
-    }
+    private val kwdFilterUpnameRegexMode = sPrefs.getBoolean("home_filter_up_regex_mode", false)
     private val kwdFilterRnameList by lazy {
         migrateHomeFilterPrefsIfNeeded()
         sPrefs.getStringSet("home_filter_keywords_category", null).orEmpty()
@@ -54,19 +42,19 @@ class PegasusHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     }
 
     private val filterMap = mapOf(
-        "advertisement" to arrayListOf("ad"),
-        "article" to arrayListOf("article"),
-        "bangumi" to arrayListOf("bangumi", "special", "pgc"),
-        "game" to arrayListOf("game"),
-        "picture" to arrayListOf("picture"),
-        "vertical" to arrayListOf("vertical", "story"),
-        "banner" to arrayListOf("banner"),
-        "live" to arrayListOf("live"),
-        "inline" to arrayListOf("inline"),
-        "notify" to arrayListOf("notify_tunnel"),
-        "large_cover" to arrayListOf("large_cover"),
-        "middle_cover" to arrayListOf("middle_cover"),
-        "small_cover" to arrayListOf("small_cover"),
+        "advertisement" to listOf("ad"),
+        "article" to listOf("article"),
+        "bangumi" to listOf("bangumi", "special", "pgc"),
+        "game" to listOf("game"),
+        "picture" to listOf("picture"),
+        "vertical" to listOf("vertical", "story"),
+        "banner" to listOf("banner"),
+        "live" to listOf("live"),
+        "inline" to listOf("inline"),
+        "notify" to listOf("notify_tunnel"),
+        "large_cover" to listOf("large_cover"),
+        "middle_cover" to listOf("middle_cover"),
+        "small_cover" to listOf("small_cover"),
     )
 
     private val filter = filterSet.flatMap {
@@ -92,33 +80,22 @@ class PegasusHook(classLoader: ClassLoader) : BaseHook(classLoader) {
 
     private fun String.isNum() = all { it.isDigit() }
 
-    private fun toLong(str: String): Long {
-        return try {
-            when {
-                str.isNum() -> return str.toDouble().toLong()
-
-                str.contains("万") ->
-                    (str.replace("万", "").toDouble() * 10_000).toLong()
-
-                str.contains("亿") ->
-                    (str.replace("亿", "").toDouble() * 100_000_000).toLong()
-
-                else -> -1L
-            }
-        } catch (e: Throwable) {
-            -1
+    private fun String.toLong() = runCatchingOrNull {
+        when {
+            isNum() -> return toDouble().toLong()
+            contains("万") -> (replace("万", "").toDouble() * 10_000).toLong()
+            contains("亿") -> (replace("亿", "").toDouble() * 100_000_000).toLong()
+            else -> -1L
         }
-    }
+    } ?: -1L
 
     // 屏蔽过低的播放数
     private fun isLowCountVideo(obj: Any): Boolean {
-        if (hideLowPlayCountLimit == 0.toLong()) return false
-        val text = try {
-            obj.getObjectField("coverLeftText1")
-        } catch (thr: Throwable) {
-            return false
-        }
-        toLong(text.toString()).let {
+        if (hideLowPlayCountLimit == 0L) return false
+        val text = obj.runCatchingOrNull {
+            getObjectFieldAs<String?>("coverLeftText1")
+        }.orEmpty()
+        text.toLong().let {
             return if (it == -1L) false
             else it < hideLowPlayCountLimit
         }
@@ -126,28 +103,23 @@ class PegasusHook(classLoader: ClassLoader) : BaseHook(classLoader) {
 
     // 屏蔽指定播放时长
     private fun durationVideo(obj: Any): Boolean {
-        val duration = try {
-            obj.getObjectField("playerArgs")?.getObjectFieldAs<Int?>("fakeDuration")!!.toInt()
-        } catch (thr: Throwable) {
+        if (hideLongDurationLimit == 0 && hideShortDurationLimit == 0)
             return false
-        }
-        if (duration > hideLongDurationLimit && hideLongDurationLimit != 0) return true
-        if (duration < hideShortDurationLimit && hideShortDurationLimit != 0) return true
+        val duration = obj.getObjectField("playerArgs")
+            ?.getObjectFieldAs("fakeDuration") ?: 0
+        if (hideLongDurationLimit != 0 && duration > hideLongDurationLimit) return true
+        if (hideShortDurationLimit != 0 && duration < hideShortDurationLimit) return true
         return false
     }
 
     private fun isContainsBlockKwd(obj: Any): Boolean {
         // 屏蔽标题关键词
         if (kwdFilterTitleList.isNotEmpty()) {
-            val title = try {
-                obj.getObjectField("title").toString()
-            } catch (thr: Throwable) {
-                return false
-            }
-            if (kwdFilterTitleRegexMode) {
+            val title = obj.getObjectFieldAs<String?>("title").orEmpty()
+            if (kwdFilterTitleRegexMode && title.isNotEmpty()) {
                 if (kwdFilterTitleRegexes.any { title.contains(it) })
                     return true
-            } else {
+            } else if (title.isNotEmpty()) {
                 if (kwdFilterTitleList.any { title.contains(it) })
                     return true
             }
@@ -155,28 +127,22 @@ class PegasusHook(classLoader: ClassLoader) : BaseHook(classLoader) {
 
         // 屏蔽UID
         if (kwdFilterUidList.isNotEmpty()) {
-            val uid = try {
-                obj.getObjectField("args")?.getObjectFieldAs("upId") ?: 0L
-            } catch (thr: Throwable) {
-                return false
-            }
-            kwdFilterUidList.forEach {
-                if (it == uid) return true
-            }
+            val uid = obj.getObjectField("args")?.getLongField("upId") ?: 0L
+            if (uid != 0L && kwdFilterUidList.any { it == uid })
+                return true
         }
 
         // 屏蔽UP主
         if (kwdFilterUpnameList.isNotEmpty()) {
-            val upname = try {
-                obj.getObjectField("args")?.getObjectField("upName").toString()
-            } catch (thr: Throwable) {
-                return false
+            val upname = if (obj.getObjectField("goTo") == "picture") {
+                obj.runCatchingOrNull { getObjectFieldAs<String?>("desc") }.orEmpty()
+            } else {
+                obj.getObjectField("args")?.getObjectFieldAs<String?>("upName").orEmpty()
             }
-            if (upname == "null") return false
-            if (kwdFilterUpnameRegexMode) {
+            if (kwdFilterUpnameRegexMode && upname.isNotEmpty()) {
                 if (kwdFilterUpnameRegexes.any { upname.contains(it) })
                     return true
-            } else {
+            } else if (upname.isNotEmpty()) {
                 if (kwdFilterUpnameList.any { upname.contains(it) })
                     return true
             }
@@ -184,111 +150,100 @@ class PegasusHook(classLoader: ClassLoader) : BaseHook(classLoader) {
 
         // 屏蔽分区
         if (kwdFilterRnameList.isNotEmpty()) {
-            val rname = try {
-                obj.getObjectField("args")?.getObjectField("rname").toString()
-            } catch (thr: Throwable) {
-                return false
-            }
-            if (rname == "null") return false
-            kwdFilterRnameList.forEach {
-                if (it.isNotEmpty() && rname.contains(it)) return true
-            }
+            val rname = obj.getObjectField("args")
+                ?.getObjectFieldAs<String?>("rname").orEmpty()
+            if (rname.isNotEmpty() && kwdFilterRnameList.any { rname.contains(it) })
+                return true
         }
 
         // 屏蔽频道
         if (kwdFilterTnameList.isNotEmpty()) {
-            val tname = try {
-                obj.getObjectField("args")?.getObjectField("tname").toString()
-            } catch (thr: Throwable) {
-                return false
-            }
-            if (tname == "null") return false
-            kwdFilterTnameList.forEach {
-                if (it.isNotEmpty() && tname.contains(it)) return true
-            }
+            val tname = obj.getObjectField("args")
+                ?.getObjectFieldAs<String?>("tname").orEmpty()
+            if (tname.isNotEmpty() && kwdFilterTnameList.any { tname.contains(it) })
+                return true
         }
 
         // 屏蔽推荐关键词（可能不存在，必须放最后）
         if (kwdFilterReasonList.isNotEmpty()) {
-            val reason = try {
-                obj.getObjectField("rcmdReason")?.getObjectField("text").toString()
-            } catch (thr: Throwable) {
-                return false
-            }
-            if (reason == "null") return false
-            if (kwdFilterReasonRegexMode) {
-                kwdFilterReasonRegexes.forEach {
-                    if (reason.contains(it)) return true
-                }
-            } else {
-                kwdFilterReasonList.forEach {
-                    if (it.isNotEmpty() && reason.contains(it)) return true
-                }
+            val reason = obj.runCatchingOrNull {
+                getObjectField("rcmdReason")?.getObjectFieldAs<String?>("text").orEmpty()
+            }.orEmpty()
+            if (kwdFilterReasonRegexMode && reason.isNotEmpty()) {
+                if (kwdFilterReasonRegexes.any { reason.contains(it) })
+                    return true
+            } else if (reason.isNotEmpty()) {
+                if (kwdFilterReasonList.any { reason.contains(it) })
+                    return true
             }
         }
 
         return false
     }
 
-    private fun ArrayList<Any>.appendReasons() {
-        for (item in this) {
-            val title = item.getObjectFieldAs<String?>("title").orEmpty()
-            val rcmdReason = item.runCatchingOrNull {
-                getObjectField("rcmdReason")?.getObjectFieldAs<String?>("text")
-            }.orEmpty()
-            val args = item.getObjectField("args")
-            val upId = args?.getLongField("upId") ?: 0L
-            val upName = if (item.getObjectField("goTo") == "picture") {
-                item.runCatchingOrNull { getObjectFieldAs<String?>("desc") }.orEmpty()
-            } else {
-                args?.getObjectFieldAs<String?>("upName").orEmpty()
+    private fun ArrayList<Any>.appendReasons() = forEach { item ->
+        val title = item.getObjectFieldAs<String?>("title").orEmpty()
+        val rcmdReason = item.runCatchingOrNull {
+            getObjectField("rcmdReason")?.getObjectFieldAs<String?>("text")
+        }.orEmpty()
+        val args = item.getObjectField("args")
+        val upId = args?.getLongField("upId") ?: 0L
+        val upName = if (item.getObjectField("goTo") == "picture") {
+            item.runCatchingOrNull { getObjectFieldAs<String?>("desc") }.orEmpty()
+        } else {
+            args?.getObjectFieldAs<String?>("upName").orEmpty()
+        }
+        val categoryName = args?.getObjectFieldAs<String?>("rname").orEmpty()
+        val channelName = args?.getObjectFieldAs<String?>("tname").orEmpty()
+        val treePoint = item.getObjectFieldAs<MutableList<Any>?>("threePoint")
+        val reasons = mutableListOf<Any>()
+        instance.treePointItemClass?.new()?.apply {
+            setObjectField("title", "漫游屏蔽")
+            setObjectField("subtitle", "(本地屏蔽，重启生效)")
+            setObjectField("type", "dislike")
+            if (title.isNotEmpty()) {
+                instance.dislikeReasonClass?.new()?.apply {
+                    setLongField("id", REASON_ID_TITLE)
+                    setObjectField("name", "标题:$title")
+                }?.let { reasons.add(it) }
             }
-            val categoryName = args?.getObjectFieldAs<String?>("rname").orEmpty()
-            val channelName = args?.getObjectFieldAs<String?>("tname").orEmpty()
-            val treePoint = item.getObjectFieldAs<MutableList<Any>?>("threePoint") ?: continue
-            instance.treePointItemClass?.new()?.apply {
-                setObjectField("title", "漫游屏蔽")
-                setObjectField("subtitle", "(本地屏蔽，重启生效)")
-                setObjectField("type", "dislike")
-                val reasons = mutableListOf<Any>()
-                if (title.isNotEmpty()) {
-                    instance.dislikeReasonClass?.new()?.apply {
-                        setLongField("id", REASON_ID_TITLE)
-                        setObjectField("name", "标题:$title")
-                    }?.let { reasons.add(it) }
-                }
-                if (rcmdReason.isNotEmpty()) {
-                    instance.dislikeReasonClass?.new()?.apply {
-                        setLongField("id", REASON_ID_RCMD_REASON)
-                        setObjectField("name", "推荐原因:$rcmdReason")
-                    }?.let { reasons.add(it) }
-                }
-                if (upId != 0L) {
-                    instance.dislikeReasonClass?.new()?.apply {
-                        setLongField("id", REASON_ID_UP_ID)
-                        setObjectField("name", "UID:$upId")
-                    }?.let { reasons.add(it) }
-                }
-                if (upName.isNotEmpty()) {
-                    instance.dislikeReasonClass?.new()?.apply {
-                        setLongField("id", REASON_ID_UP_NAME)
-                        setObjectField("name", "UP主:$upName")
-                    }?.let { reasons.add(it) }
-                }
-                if (categoryName.isNotEmpty()) {
-                    instance.dislikeReasonClass?.new()?.apply {
-                        setLongField("id", REASON_ID_CATEGORY_NAME)
-                        setObjectField("name", "分区:$categoryName")
-                    }?.let { reasons.add(it) }
-                }
-                if (channelName.isNotEmpty()) {
-                    instance.dislikeReasonClass?.new()?.apply {
-                        setLongField("id", REASON_ID_CHANNEL_NAME)
-                        setObjectField("name", "频道:$channelName")
-                    }?.let { reasons.add(it) }
-                }
-                setObjectField("reasons", reasons)
-            }?.let { treePoint.add(it) }
+            if (rcmdReason.isNotEmpty()) {
+                instance.dislikeReasonClass?.new()?.apply {
+                    setLongField("id", REASON_ID_RCMD_REASON)
+                    setObjectField("name", "推荐原因:$rcmdReason")
+                }?.let { reasons.add(it) }
+            }
+            if (upId != 0L) {
+                instance.dislikeReasonClass?.new()?.apply {
+                    setLongField("id", REASON_ID_UP_ID)
+                    setObjectField("name", "UID:$upId")
+                }?.let { reasons.add(it) }
+            }
+            if (upName.isNotEmpty()) {
+                instance.dislikeReasonClass?.new()?.apply {
+                    setLongField("id", REASON_ID_UP_NAME)
+                    setObjectField("name", "UP主:$upName")
+                }?.let { reasons.add(it) }
+            }
+            if (categoryName.isNotEmpty()) {
+                instance.dislikeReasonClass?.new()?.apply {
+                    setLongField("id", REASON_ID_CATEGORY_NAME)
+                    setObjectField("name", "分区:$categoryName")
+                }?.let { reasons.add(it) }
+            }
+            if (channelName.isNotEmpty()) {
+                instance.dislikeReasonClass?.new()?.apply {
+                    setLongField("id", REASON_ID_CHANNEL_NAME)
+                    setObjectField("name", "频道:$channelName")
+                }?.let { reasons.add(it) }
+            }
+            setObjectField("reasons", reasons)
+        }?.let {
+            if (treePoint != null && reasons.isNotEmpty()) {
+                treePoint.add(it)
+            } else if (reasons.isNotEmpty()) {
+                item.setObjectField("threePoint", mutableListOf(it))
+            }
         }
     }
 
@@ -300,11 +255,11 @@ class PegasusHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         ) { param ->
             param.result.getObjectField("data")?.getObjectFieldAs<ArrayList<Any>>("items")?.run {
                 removeAll {
+                    val cardGoto = it.getObjectFieldAs<String?>("cardGoto").orEmpty()
+                    val cardType = it.getObjectFieldAs<String?>("cardType").orEmpty()
+                    val goto = it.getObjectFieldAs<String?>("goTo").orEmpty()
                     filter.any { item ->
-                        item in it.getObjectFieldOrNullAs<String>("cardGoto")
-                            .orEmpty() || item in it.getObjectFieldOrNullAs<String>("cardType")
-                            .orEmpty() || item in it.getObjectFieldOrNullAs<String>("goTo")
-                            .orEmpty()
+                        item in cardGoto || item in cardType || item in goto
                     } || isLowCountVideo(it) || isContainsBlockKwd(it) || durationVideo(it)
                 }
                 appendReasons()
