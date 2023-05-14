6db90396d6189aa6d44f6c3f5a74d361f3e90e5e
Kofua <1638183271zjn@gmail.com>
2023-05-14 12:31:37 +0800

    Fix save comment images for 7.29.0+ (#1095)
    新版本这一块改用 Jetpack Compose，很难直接添加长按事件，
    因此需要点开后长按预览图片保存

--- app/src/main/java/me/iacn/biliroaming/hook/CommentImageHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/CommentImageHook.kt
@@ -2,11 +2,12 @@ package me.iacn.biliroaming.hook
 
 import android.content.ContentValues
 import android.os.Build
+import android.os.Bundle
 import android.os.Environment
+import android.os.Parcelable
 import android.provider.MediaStore
+import android.view.HapticFeedbackConstants
 import android.view.View
-import android.widget.ImageView
-import de.robv.android.xposed.XC_MethodHook
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
@@ -18,104 +19,74 @@ import java.net.URL
 
 class CommentImageHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     companion object {
-        fun saveImage(url: String) {
-            runCatching {
-                URL(url).openStream().use { stream ->
-                    val relativePath =
-                        "${Environment.DIRECTORY_PICTURES}${File.separator}bilibili"
-                    val fullFilename = url.substringAfterLast('/')
-                    val filename = fullFilename.substringBeforeLast('.')
+        fun saveImage(url: String) = runCatching {
+            URL(url).openStream().use { stream ->
+                val relativePath = "${Environment.DIRECTORY_PICTURES}${File.separator}bilibili"
+                val fullFilename = url.substringAfterLast('/')
+                val filename = fullFilename.substringBeforeLast('.')
 
-                    val now = System.currentTimeMillis()
-                    val contentValues = ContentValues().apply {
-                        put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
-                        put(
-                            MediaStore.MediaColumns.MIME_TYPE,
-                            HttpURLConnection.guessContentTypeFromName(fullFilename)
-                                ?: "image/png"
-                        )
-                        put(MediaStore.MediaColumns.DATE_ADDED, now / 1000)
-                        put(MediaStore.MediaColumns.DATE_MODIFIED, now / 1000)
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                            put(MediaStore.MediaColumns.DATE_TAKEN, now)
-                            put(MediaStore.MediaColumns.RELATIVE_PATH, relativePath)
-                        } else {
-                            val path = File(
-                                Environment.getExternalStoragePublicDirectory(
-                                    Environment.DIRECTORY_PICTURES
-                                ), "bilibili"
-                            ).also { it.mkdirs() }
-                            put(
-                                MediaStore.MediaColumns.DATA,
-                                File(path, fullFilename).absolutePath
-                            )
-                        }
-                    }
-                    val resolver = currentContext.contentResolver
-                    val uri = resolver.insert(
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+                val now = System.currentTimeMillis()
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(
+                        MediaStore.MediaColumns.MIME_TYPE,
+                        HttpURLConnection.guessContentTypeFromName(fullFilename) ?: "image/png"
                     )
-                    runCatching {
-                        resolver.openOutputStream(uri!!)?.use { stream.copyTo(it) }
-                    }.onSuccess {
-                        Log.toast(
-                            "图片已保存至\n$relativePath${File.separator}$fullFilename",
-                            true
-                        )
-                    }.onFailure {
-                        Log.e(it)
-                        Log.toast("图片保存失败，可能已经保存或未授予权限", true)
+                    put(MediaStore.MediaColumns.DATE_ADDED, now / 1000)
+                    put(MediaStore.MediaColumns.DATE_MODIFIED, now / 1000)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        put(MediaStore.MediaColumns.DATE_TAKEN, now)
+                        put(MediaStore.MediaColumns.RELATIVE_PATH, relativePath)
+                    } else {
+                        val path = File(
+                            Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_PICTURES
+                            ), "bilibili"
+                        ).also { it.mkdirs() }
+                        put(MediaStore.MediaColumns.DATA, File(path, fullFilename).absolutePath)
                     }
                 }
-            }.onFailure {
-                Log.e(it)
-                Log.toast("图片获取失败", force = true)
+                val resolver = currentContext.contentResolver
+                val uri = resolver.insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+                )
+                runCatching {
+                    resolver.openOutputStream(uri!!)?.use { stream.copyTo(it) }
+                }.onSuccess {
+                    Log.toast("图片已保存至\n$relativePath${File.separator}$fullFilename", true)
+                }.onFailure {
+                    Log.e(it)
+                    Log.toast("图片保存失败，可能已经保存或未授予权限", true)
+                }
             }
+        }.onFailure {
+            Log.e(it)
+            Log.toast("图片获取失败", force = true)
         }
     }
 
-    @Suppress("UNCHECKED_CAST")
+    private val imageViewId = getId("image_view")
+    private var cacheUrlFieldName = ""
+
+    @Suppress("DEPRECATION")
     override fun startHook() {
         if (!sPrefs.getBoolean("save_comment_image", false)) return
-        val hooker = fun(param: XC_MethodHook.MethodHookParam) {
-            val images = param.args[1] as? List<ImageView> ?: return
-            val imageUrls = if (param.args.size > 3) {
-                (param.args[3] as? List<Any>)?.map { it.getFirstFieldByExactTypeOrNull() }
-            } else param.args[2].getObjectFieldOrNullAs<List<String?>?>("images")
-            imageUrls ?: return
-            for ((image, imageUrl) in images.zip(imageUrls)) {
-                imageUrl ?: continue
-                image.setOnLongClickListener {
-                    MainScope().launch(Dispatchers.IO) {
-                        saveImage(imageUrl)
-                    }
-                    true
-                }
-            }
-        }
-        instance.commentImageLoaderClass?.declaredMethods?.filter {
-            it.name == instance.load() && it.parameterTypes.size == 6
-                    || it.name == instance.richLoad() && it.parameterTypes.size == 3
-        }?.forEach { it.hookAfterMethod(hooker) }
-        instance.cardGridViewBinderClass?.declaredMethods?.find { it.name == instance.bind() }
-            ?.hookAfterMethod { param ->
-                val pos = param.args[1] as Int
-                val imageUrl = param.thisObject
-                    .getObjectFieldOrNullAs<List<Any>?>(instance.dataInterfacesField())
-                    ?.getOrNull(pos)?.getFirstFieldByExactTypeOrNull<String>()
-                    ?: return@hookAfterMethod
-                param.args[0]?.getFirstFieldByExactTypeOrNull<View>()?.setOnLongClickListener {
-                    MainScope().launch(Dispatchers.IO) {
-                        saveImage(imageUrl)
-                    }
-                    true
-                }
-            }
-        instance.roundImageViewClass?.declaredMethods?.find { m ->
-            m.parameterTypes.let { ts -> ts.size == 2 && ts.all { it == String::class.java } }
-        }?.hookAfterMethod { param ->
-            val imageUrl = (param.args[0] as String).ifBlank { return@hookAfterMethod }
-            (param.thisObject as View).setOnLongClickListener {
+        instance.imageFragmentClass?.hookAfterMethod(
+            "onViewCreated", View::class.java, Bundle::class.java
+        ) { param ->
+            val self = param.thisObject
+            val view = param.args[0] as? View
+            val imageItem = self.callMethodOrNullAs<Bundle?>("getArguments")
+                ?.getParcelable<Parcelable>("image_item") ?: return@hookAfterMethod
+            val urlFieldName = cacheUrlFieldName.ifEmpty {
+                imageItem.javaClass.superclass.findFirstFieldByExactTypeOrNull(String::class.java)
+                    ?.name.orEmpty().also { cacheUrlFieldName = it }
+            }.ifEmpty { return@hookAfterMethod }
+            val imageUrl = imageItem.getObjectFieldAs<String?>(urlFieldName).takeIf {
+                !it.isNullOrEmpty() && it.startsWith("http")
+            }?.substringBefore('@') ?: return@hookAfterMethod
+            view?.findViewById<View>(imageViewId)?.setOnLongClickListener {
+                it.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS)
                 MainScope().launch(Dispatchers.IO) {
                     saveImage(imageUrl)
                 }
