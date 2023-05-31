3aadb6c2146a2e57ca40673568efb58d9a10bd9b
陈寒彤 <70561268+cxw620@users.noreply.github.com>
2023-05-31 23:33:01 +0800

    fix: disable player long press acceleration on 7.32.0+ (#1137)
    fix: 7.32.0版本禁用长按倍速播放失效 #1136

--- app/src/main/java/me/iacn/biliroaming/hook/PlayerLongPressHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/PlayerLongPressHook.kt
@@ -7,23 +7,25 @@ class PlayerLongPressHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
         if (!sPrefs.getBoolean("forbid_player_long_click_accelerate", false)) return
 
-        Log.d("startHook: PlayerLongPress");
+        Log.d("startHook: PlayerLongPress")
 
         val hooker: Hooker = { param -> param.result = true }
         // pre 6.59.0
         "tv.danmaku.biliplayerimpl.gesture.GestureService\$mTouchListener\$1".findClassOrNull(
             mClassLoader
         )?.hookBeforeMethod(
-            "onLongPress",
-            MotionEvent::class.java,
-            hooker = hooker
+            "onLongPress", MotionEvent::class.java, hooker = hooker
         )
         // post 6.59.0
         arrayOf(
             "tv.danmaku.biliplayerimpl.gesture.GestureService\$initInnerLongPressListener\$1\$onLongPress\$1",
-            "tv.danmaku.biliplayerimpl.gesture.GestureService\$initInnerLongPressListener\$1\$onLongPressEnd\$1"
+            "tv.danmaku.biliplayerimpl.gesture.GestureService\$initInnerLongPressListener\$1\$onLongPressEnd\$1",
+            // post 7.32.0
+            "com.bilibili.playerbizcommon.gesture.GestureService\$initInnerLongPressListener\$1\$onLongPress\$1",
+            "com.bilibili.playerbizcommon.gesture.GestureService\$initInnerLongPressListener\$1\$onLongPressEnd\$1"
         ).forEach { className ->
-            className.hookBeforeMethod(mClassLoader, "invoke", Object::class.java, hooker = hooker)
+            className.findClassOrNull(mClassLoader)
+                ?.hookBeforeMethod("invoke", Object::class.java, hooker = hooker)
         }
     }
 }
