2c8d8b1ba59220e384de29f6996ee0395d349ea8
LoveSy <shana@zju.edu.cn>
2023-07-02 15:49:02 +0800

    Fix #1160

--- app/src/main/java/me/iacn/biliroaming/hook/StartActivityHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/StartActivityHook.kt
@@ -1,16 +1,24 @@
 package me.iacn.biliroaming.hook
 
+import android.app.Activity
 import android.app.Instrumentation
 import android.content.ComponentName
 import android.content.Intent
 import android.net.Uri
+import android.os.Bundle
 import me.iacn.biliroaming.utils.Log
 import me.iacn.biliroaming.utils.hookBeforeAllMethods
+import me.iacn.biliroaming.utils.hookBeforeMethod
 import me.iacn.biliroaming.utils.packageName
 import me.iacn.biliroaming.utils.sPrefs
 
 class StartActivityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
+        "tv.danmaku.bili.ui.intent.IntentHandlerActivity".hookBeforeMethod(mClassLoader, "onCreate", Bundle::class.java) { param ->
+            val a = param.thisObject as Activity
+            val data = a.intent.data ?: return@hookBeforeMethod
+            a.intent.data = data.buildUpon().encodedQuery(data.encodedQuery?.replace("&-Arouter=story", "")).build()
+        }
         Instrumentation::class.java.hookBeforeAllMethods("execStartActivity") { param ->
             val intent = param.args[4] as? Intent ?: return@hookBeforeAllMethods
             val uri = intent.dataString ?: return@hookBeforeAllMethods
