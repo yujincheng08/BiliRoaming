e3508fc13b9ada8dbfadd3291003087d1a39a512
LoveSy <shana@zju.edu.cn>
2024-01-11 22:37:30 +0800

    Fix typo

--- app/src/main/java/me/iacn/biliroaming/hook/PlayArcConfHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/PlayArcConfHook.kt
@@ -45,7 +45,7 @@ class PlayArcConfHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         ) { param ->
             param.result?.callMethod("mergePlayArcConf", arcConfs)
         }
-        instance.playerMossClass?.hookAfterMethod("playViewUnite", instance.mossResponseHandlerClass) { param ->
+        instance.playerMossClass?.hookAfterMethod("playViewUnite", instance.playViewUniteReqClass, instance.mossResponseHandlerClass) { param ->
             param.args[1] = param.args[1].mossResponseHandlerProxy { resp ->
                 resp?.callMethod("mergePlayArcConf", arcConfs)
             }
