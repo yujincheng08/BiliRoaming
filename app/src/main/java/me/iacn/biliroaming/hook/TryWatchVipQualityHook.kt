1f2f8bfe6c76498ba0d109ffc12ed9b45987df7f
TinyHai <34483077+TinyHai@users.noreply.github.com>
2025-01-09 22:55:36 +0800

    fix: disable try vip quality for v7.50.0+ (#1609)
    - fix #1360

--- app/src/main/java/me/iacn/biliroaming/hook/TryWatchVipQualityHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/TryWatchVipQualityHook.kt
@@ -3,19 +3,13 @@ package me.iacn.biliroaming.hook
 import me.iacn.biliroaming.BiliBiliPackage.Companion.instance
 import me.iacn.biliroaming.utils.Log
 import me.iacn.biliroaming.utils.hookBeforeMethod
+import me.iacn.biliroaming.utils.replaceMethod
 import me.iacn.biliroaming.utils.sPrefs
 
 class TryWatchVipQualityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
         if (!sPrefs.getBoolean("disable_try_watch_vip_quality", false)) return
 
-        Log.d("startHook: TryWatchVipQualityHook")
-        instance.canTryWatchVipQuality()?.let { m ->
-            instance.playerQualityServiceClass?.hookBeforeMethod(
-                m
-            ) {
-                it.result = false
-            }
-        }
+        instance.vipQualityTrialService?.replaceMethod(instance.canTrialMethod()) { false }
     }
 }
