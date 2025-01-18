d6ee564493a8e8951739192a26c56679dc43d5ac
TinyHai <1035908357@qq.com>
2025-01-18 20:02:48 +0800

    fix: refine live quality

--- app/src/main/java/me/iacn/biliroaming/hook/LiveQualityHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/LiveQualityHook.kt
@@ -2,11 +2,25 @@ package me.iacn.biliroaming.hook
 
 import android.net.Uri
 import me.iacn.biliroaming.BiliBiliPackage.Companion.instance
+import me.iacn.biliroaming.BuildConfig
 import me.iacn.biliroaming.utils.*
 import org.json.JSONArray
 import org.json.JSONObject
 
 class LiveQualityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
+
+    companion object {
+        private const val TAG = "LiveQualityHook"
+    }
+
+    @Volatile
+    private var newQn: String = ""
+
+    private fun debug(msg: () -> String) {
+        if (BuildConfig.DEBUG) {
+            Log.d("[$TAG] - ${msg()}")
+        }
+    }
 
     override fun startHook() {
         val liveQuality = sPrefs.getString("live_quality", "0")?.toIntOrNull() ?: 0
@@ -15,6 +29,36 @@ class LiveQualityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         }
 
         val canSwitchLiveRoom = !sPrefs.getBoolean("forbid_switch_live_room", false)
+
+        instance.defaultRequestInterceptClass?.hookBeforeAllMethods(instance.interceptMethod()) { param ->
+            val request = param.args[0]
+            val httpUrl = request.getObjectField(instance.urlField())
+            val url = httpUrl.toString()
+            if (!url.startsWith("https://api.live.bilibili.com/xlive/app-room/v2/index/getRoomPlayInfo?")) {
+                return@hookBeforeAllMethods
+            }
+
+            debug { "oldHttpUrl: $url" }
+
+            val uri = Uri.parse(httpUrl.toString())
+            val qn = uri.getQueryParameter("qn")
+            if (qn.isNullOrEmpty() || qn == "0") {
+                val builder = uri.buildUpon().clearQuery()
+                for (name in uri.queryParameterNames) {
+                    if (name == "qn") {
+                        builder.appendQueryParameter(name, newQn.ifEmpty { liveQuality.toString() })
+                    } else {
+                        builder.appendQueryParameter(name, uri.getQueryParameter(name))
+                    }
+                }
+                val newHttpUrl = instance.httpUrlClass?.callStaticMethod(
+                    instance.httpUrlParseMethod(),
+                    builder.build().toString()
+                )
+                debug { "newHttpUrl: $newHttpUrl" }
+                request.setObjectField(instance.urlField(), newHttpUrl)
+            }
+        }
 
         instance.retrofitResponseClass?.hookBeforeAllConstructors { param ->
             val url = getRetrofitUrl(param.args[0]) ?: return@hookBeforeAllConstructors
@@ -25,28 +69,27 @@ class LiveQualityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                 // 处理上下滑动切换直播间
                 url.startsWith("https://api.live.bilibili.com/xlive/app-interface/v2/room/recList?") && canSwitchLiveRoom -> {
                     val data = body.getObjectField("data") ?: return@hookBeforeAllConstructors
-                    val info = JSONObject(instance.fastJsonClass?.callStaticMethod("toJSONString", data).toString())
+                    val info = JSONObject(
+                        instance.fastJsonClass?.callStaticMethod("toJSONString", data).toString()
+                    )
                     if (fixLiveRoomFeedInfo(info, liveQuality)) {
                         body.setObjectField(
                             "data",
-                            instance.fastJsonClass?.callStaticMethod(instance.fastJsonParse(), info.toString(), data.javaClass)
+                            instance.fastJsonClass?.callStaticMethod(
+                                instance.fastJsonParse(),
+                                info.toString(),
+                                data.javaClass
+                            )
                         )
                     }
                 }
-                url.startsWith("https://api.live.bilibili.com/xlive/app-room/v2/index/getRoomPlayInfo?") -> {
-                    val uri = Uri.parse(url)
-                    val reqQn = uri.getQueryParameter("qn")
-                    if (!reqQn.isNullOrEmpty() && reqQn != "0") {
-                        return@hookBeforeAllConstructors
-                    }
+
+                BuildConfig.DEBUG && url.startsWith("https://api.live.bilibili.com/xlive/app-room/v2/index/getRoomPlayInfo?") -> {
                     val data = body.getObjectField("data") ?: return@hookBeforeAllConstructors
-                    val info = JSONObject(instance.fastJsonClass?.callStaticMethod("toJSONString", data).toString())
-                    if (fixRoomPlayInfo(info, liveQuality)) {
-                        body.setObjectField(
-                            "data",
-                            instance.fastJsonClass?.callStaticMethod(instance.fastJsonParse(), info.toString(), data.javaClass)
-                        )
-                    }
+                    val info = JSONObject(
+                        instance.fastJsonClass?.callStaticMethod("toJSONString", data).toString()
+                    )
+                    printCodec(info)
                 }
             }
         }
@@ -59,7 +102,7 @@ class LiveQualityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         }
 
         instance.livePlayUrlSelectUtilClass?.hookBeforeMethod(
-            instance.parseUriMethod(),
+            instance.buildSelectorDataMethod(),
             Uri::class.java
         ) { param ->
             val originalUri = param.args[0] as Uri
@@ -67,18 +110,18 @@ class LiveQualityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                 return@hookBeforeMethod
             }
 
-            val newQuality = findQuality(
+            debug { "originalLiveUrl: $originalUri" }
+
+            newQn = findQuality(
                 JSONArray(originalUri.getQueryParameter("accept_quality")),
                 liveQuality
             ).toString()
+            debug { "newQn: $newQn" }
 
-            param.args[0] = originalUri.replaceQuery { name, oldVal ->
-                when {
-                    "current_qn" == name -> newQuality
-                    "current_quality" == name -> newQuality
-                    name.endsWith("current_qn") -> "0"
-                    else -> oldVal
-                }
+            param.args[0] = originalUri.removeQuery { name ->
+                name.startsWith("playurl")
+            }.also {
+                debug { "newLiveUrl: $it" }
             }
         }
     }
@@ -100,11 +143,13 @@ class LiveQualityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
         }
     }
 
-    private fun Uri.replaceQuery(replacer: (String, String) -> String?): Uri {
+    private fun Uri.removeQuery(predicate: (String) -> Boolean): Uri {
         val newBuilder = buildUpon().clearQuery()
         for (name in queryParameterNames) {
-            val newValue = replacer(name, getQueryParameter(name) ?: "")
-            newValue?.let { newBuilder.appendQueryParameter(name, it) }
+            val value = getQueryParameter(name) ?: ""
+            if (!predicate(name)) {
+                newBuilder.appendQueryParameter(name, value)
+            }
         }
         return newBuilder.build()
     }
@@ -112,36 +157,31 @@ class LiveQualityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     private fun fixLiveRoomFeedInfo(info: JSONObject, expectQuality: Int): Boolean {
         val feedList = info.optJSONArray("list") ?: return false
         feedList.iterator().forEach { feedData ->
+            debug { "oldFeedData: ${feedData.toString(2)}" }
             val newQuality = findQuality(feedData.getJSONArray("accept_quality"), expectQuality)
-            feedData.put("current_qn", newQuality)
-            feedData.put("current_quality", newQuality)
+            feedData.apply {
+                put("current_qn", newQuality)
+                put("current_quality", newQuality)
+                put("play_url", "")
+                put("play_url_h265", "")
+                put("playurl_infos", JSONArray())
+            }
+            debug { "newFeedData: ${feedData.toString(2)}" }
         }
         return true
     }
 
-    private fun fixRoomPlayInfo(info: JSONObject, expectQuality: Int): Boolean {
-        val playUrlInfo = info.optJSONObject("playurl_info") ?: return false
+    private fun printCodec(info: JSONObject) {
+        val playUrlInfo = info.optJSONObject("playurl_info") ?: return
         val playUrlObj = playUrlInfo.getJSONObject("playurl")
+        debug { "printCodec >>>>>>>>>>>>>>" }
         playUrlObj.getJSONArray("stream").iterator().forEach { stream ->
             stream.getJSONArray("format").iterator().forEach { format ->
-                format.getJSONArray("codec").iterator().asSequence().run {
-                    firstOrNull { codec -> fixCodec(codec, expectQuality, true) }
-                        ?: firstOrNull { codec ->  fixCodec(codec, expectQuality, false) }
+                format.getJSONArray("codec").iterator().forEach {
+                    debug { "codec: ${it.toString(2)}" }
                 }
             }
         }
-        return true
-    }
-
-    private fun fixCodec(codec: JSONObject, expectQuality: Int, strict: Boolean): Boolean {
-        val newQuality = findQuality(codec.getJSONArray("accept_qn"), expectQuality)
-        if (strict && newQuality != expectQuality) {
-            return false
-        }
-        val oldQn = codec.getInt("current_qn")
-        if (oldQn != newQuality) {
-            codec.put("current_qn", newQuality)
-        }
-        return true
+        debug { "printCodec <<<<<<<<<<<<<<" }
     }
 }
