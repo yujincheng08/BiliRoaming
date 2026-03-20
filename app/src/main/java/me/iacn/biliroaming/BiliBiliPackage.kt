04b7aaab295f1873c55e9285d58cce79c9389388
U29kaXVt <77036672+U29kaXVt@users.noreply.github.com>
2026-03-20 19:44:21 +0800

    fix: CopyCommentHook.kt
    使用正确的实现方式修复自由复制

--- app/src/main/java/me/iacn/biliroaming/BiliBiliPackage.kt
+++ app/src/main/java/me/iacn/biliroaming/BiliBiliPackage.kt
@@ -87,6 +87,7 @@ class BiliBiliPackage constructor(private val mClassLoader: ClassLoader, mContex
     val commentCopyClass by Weak { mHookInfo.commentLongClick from mClassLoader }
     val commentCopyNewClass by Weak { mHookInfo.commentLongClickNew from mClassLoader }
     val comment3CopyClass by Weak { mHookInfo.comment3Copy.class_ from mClassLoader }
+    val commentMenuCopyClass by Weak { mHookInfo.commentMenuCopy.class_ from mClassLoader }
     val kotlinJsonClass by Weak { "kotlinx.serialization.json.Json" from mClassLoader }
     val gsonConverterClass by Weak { mHookInfo.gsonHelper.gsonConverter from mClassLoader }
     val playerCoreServiceV2Class by Weak { mHookInfo.playerCoreService.class_ from mClassLoader }
@@ -282,6 +283,8 @@ class BiliBiliPackage constructor(private val mClassLoader: ClassLoader, mContex
     fun comment3Copy() = mHookInfo.comment3Copy.method.orNull
 
     fun comment3ViewIndex() = mHookInfo.comment3Copy.comment3ViewIndex
+
+    fun commentMenuCopyActionHandler() = mHookInfo.commentMenuCopy.actionHandler.orNull
 
     fun responseDataField() = runCatchingOrNull {
         rxGeneralResponseClass?.getDeclaredField("data")?.name
@@ -1637,6 +1640,20 @@ class BiliBiliPackage constructor(private val mClassLoader: ClassLoader, mContex
                     comment3ViewIndex = it.parameterCount - 1
                 }
             }
+            commentMenuCopy = commentMenuCopy {
+                val helperMethod = dexHelper.findMethodUsingString(
+                    "CommentHolderHelper",
+                    false, -1, -1, null, -1, null, null, null, false
+                ).asSequence().mapNotNull { dexHelper.decodeMethodIndex(it) }
+                    .firstOrNull() ?: return@commentMenuCopy
+                val helperClass = helperMethod.declaringClass
+                val actionHandlerMethod = helperClass.declaredMethods.firstOrNull {
+                    it.isStatic && it.parameterCount == 10 &&
+                            it.parameterTypes.last().isEnum
+                } ?: return@commentMenuCopy
+                class_ = class_ { name = helperClass.name }
+                actionHandler = method { name = actionHandlerMethod.name }
+            }
             dexHelper.findMethodUsingString(
                 "BangumiAllButton",
                 true, -1, 0, null, -1, null, null, null, true
@@ -2298,8 +2315,13 @@ class BiliBiliPackage constructor(private val mClassLoader: ClassLoader, mContex
                     true
                 ).asSequence().mapNotNull { dexHelper.decodeMethodIndex(it) }.firstOrNull()
                     ?: return@preBuiltConfig
-                class_ = class_ { name = getMap.declaringClass.name }
-                get = method { name = getMap.name }
+                val actualMethod = if (getMap.isStatic) {
+                    getMap.declaringClass.declaredMethods.firstOrNull {
+                        it.isNotStatic && it.parameterTypes.isEmpty() && it.returnType == Map::class.java
+                    } ?: return@preBuiltConfig
+                } else getMap
+                class_ = class_ { name = actualMethod.declaringClass.name }
+                get = method { name = actualMethod.name }
             }
             dataSP = dataSP {
                 val spxClass =
@@ -2330,8 +2352,13 @@ class BiliBiliPackage constructor(private val mClassLoader: ClassLoader, mContex
                     Log.d(it)
                     it.declaringClass.name.startsWith("com.bilibili.lib.blconfig.internal.TypedContext")
                 } ?: return@dataSP
-                class_ = class_ { name = getDataSP.declaringClass.name }
-                get = method { name = getDataSP.name }
+                val actualMethod = if (getDataSP.isStatic) {
+                    getDataSP.declaringClass.declaredMethods.firstOrNull {
+                        it.isNotStatic && it.parameterTypes.isEmpty() && it.returnType == spxClass
+                    } ?: return@dataSP
+                } else getDataSP
+                class_ = class_ { name = actualMethod.declaringClass.name }
+                get = method { name = actualMethod.name }
             }
             pegasusParser = class_ {
                 val getPegasusParser = dexHelper.findMethodUsingString(
--- app/src/main/java/me/iacn/biliroaming/hook/CopyCommentHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/CopyCommentHook.kt
@@ -1,46 +1,43 @@
 package me.iacn.biliroaming.hook
 
 import android.app.AlertDialog
+import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.view.View
 import android.widget.TextView
-import me.iacn.biliroaming.utils.currentContext
+import me.iacn.biliroaming.BiliBiliPackage.Companion.instance
 import me.iacn.biliroaming.utils.getResId
-import me.iacn.biliroaming.utils.hookMethod
+import me.iacn.biliroaming.utils.hookAllMethods
 import me.iacn.biliroaming.utils.sPrefs
 
 
 class CopyCommentHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
         if (!sPrefs.getBoolean("copy_comment", false)) return
-        "com.bilibili.app.comment3.ui.widget.menu.CommentMoreMenuItemHolder".hookMethod(
-            mClassLoader,
-            "y3",
-            "kotlin.jvm.functions.Function1",
-            "com.bilibili.app.comment3.data.model.CommentItem\$MenuItem",
-            "android.view.View",
-        ) { chain ->
-            val result = chain.proceed()
-            val menu = chain.args[1]
-            val view = chain.args[2] as View
-            if (!menu.toString().contains("COPY")) return@hookMethod result
+        val helperClass = instance.commentMenuCopyClass ?: return
+        val methodName = instance.commentMenuCopyActionHandler() ?: return
+        helperClass.hookAllMethods(methodName) { chain ->
+            val action = chain.args.last()
+            if (action.toString() != "COPY") return@hookAllMethods chain.proceed()
 
-            val clipboard =
-                currentContext.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            // 实在是找不到了。那你就说有没有获取到吧
-            val txt = clipboard.primaryClip!!.getItemAt(0).text
-            AlertDialog.Builder(view.context, getResId("AppTheme.Dialog.Alert", "style"))
+            val context = chain.args[0] as Context
+            val txt = chain.args[7] as? String ?: return@hookAllMethods chain.proceed()
+
+            AlertDialog.Builder(context, getResId("AppTheme.Dialog.Alert", "style"))
                 .run {
                     setTitle("自由复制内容")
                     setMessage(txt)
                     setPositiveButton("完成") { _, _ -> }
-                    setNegativeButton("复制全部") { _, _ -> }
+                    setNegativeButton("复制全部") { _, _ ->
+                        val clipboard =
+                            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                        clipboard.setPrimaryClip(ClipData.newPlainText("comment", txt))
+                    }
                     show()
                 }.apply {
                     findViewById<TextView>(android.R.id.message).setTextIsSelectable(true)
                 }
-            result
+            null
         }
     }
 }
